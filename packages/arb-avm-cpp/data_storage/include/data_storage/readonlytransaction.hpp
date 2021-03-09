Dhanush123/arbitrum

Base DB refactor

packages/arb-avm-cpp/data_storage/include/data_storage/readonlytransaction.hpp
@@ -1,0 +1,119 @@
+/*
+ * Copyright 2021, Offchain Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef data_storage_readonlytransaction_hpp
+#define data_storage_readonlytransaction_hpp
+
+#include <data_storage/datastorage.hpp>
+
+class ReadWriteTransaction;
+
+class ReadOnlyTransaction {
+   private:
+    std::unique_ptr<Transaction> transaction{};
+    rocksdb::ReadOptions read_options{};
+
+    friend ReadWriteTransaction;
+
+   public:
+    explicit ReadOnlyTransaction(std::shared_ptr<DataStorage> store);
+    ~ReadOnlyTransaction();
+
+    static std::unique_ptr<ReadOnlyTransaction> makeReadOnlyTransaction(
+        std::shared_ptr<DataStorage> store);
+
+    void enableReadSnapshot();
+    void disableReadSnapshot();
+
+    rocksdb::Status defaultGet(const rocksdb::Slice& key,
+                               std::string* value) const;
+    rocksdb::Status stateGet(const rocksdb::Slice& key,
+                             std::string* value) const;
+    rocksdb::Status blocksGet(const rocksdb::Slice& key,
+                              std::string* value) const;
+    rocksdb::Status nodeGet(const rocksdb::Slice& key,
+                            std::string* value) const;
+    rocksdb::Status checkpointGet(const rocksdb::Slice& key,
+                                  std::string* value) const;
+    rocksdb::Status messageEntryGet(const rocksdb::Slice& key,
+                                    std::string* value) const;
+    rocksdb::Status logGet(const rocksdb::Slice& key, std::string* value) const;
+    rocksdb::Status sendGet(const rocksdb::Slice& key,
+                            std::string* value) const;
+    rocksdb::Status sideloadGet(const rocksdb::Slice& key,
+                                std::string* value) const;
+    [[nodiscard]] std::unique_ptr<rocksdb::Iterator> defaultGetIterator() const;
+    [[nodiscard]] std::unique_ptr<rocksdb::Iterator> stateGetIterator() const;
+    [[nodiscard]] std::unique_ptr<rocksdb::Iterator> blocksGetIterator() const;
+    [[nodiscard]] std::unique_ptr<rocksdb::Iterator> nodeGetIterator() const;
+    [[nodiscard]] std::unique_ptr<rocksdb::Iterator> checkpointGetIterator()
+        const;
+    [[nodiscard]] std::unique_ptr<rocksdb::Iterator> messageEntryGetIterator()
+        const;
+    [[nodiscard]] std::unique_ptr<rocksdb::Iterator> logGetIterator() const;
+    [[nodiscard]] std::unique_ptr<rocksdb::Iterator> sendGetIterator() const;
+    [[nodiscard]] std::unique_ptr<rocksdb::Iterator> sideloadGetIterator()
+        const;
+
+    [[nodiscard]] ValueResult<uint256_t> defaultGetUint256(
+        rocksdb::Slice key_slice) const;
+    [[nodiscard]] ValueResult<uint256_t> stateGetUint256(
+        rocksdb::Slice key_slice) const;
+    [[nodiscard]] ValueResult<uint256_t> blocksGetUint256(
+        rocksdb::Slice key_slice) const;
+    [[nodiscard]] ValueResult<uint256_t> nodeGetUint256(
+        rocksdb::Slice key_slice) const;
+    [[nodiscard]] ValueResult<uint256_t> checkpointGetUint256(
+        rocksdb::Slice key_slice) const;
+    [[nodiscard]] ValueResult<uint256_t> messageEntryGetUint256(
+        rocksdb::Slice key_slice) const;
+    [[nodiscard]] ValueResult<uint256_t> logGetUint256(
+        rocksdb::Slice key_slice) const;
+    [[nodiscard]] ValueResult<uint256_t> sendGetUint256(
+        rocksdb::Slice key_slice) const;
+    [[nodiscard]] ValueResult<uint256_t> sideloadGetUint256(
+        rocksdb::Slice key_slice) const;
+    [[nodiscard]] ValueResult<std::vector<uint256_t>> logGetUint256Vector(
+        rocksdb::Slice first_key_slice,
+        size_t count) const;
+    [[nodiscard]] ValueResult<std::vector<std::vector<unsigned char>>>
+    messageEntryGetVectorVector(rocksdb::Slice first_key_slice,
+                                size_t count) const;
+    [[nodiscard]] ValueResult<std::vector<std::vector<unsigned char>>>
+    sendGetVectorVector(rocksdb::Slice first_key_slice, size_t count) const;
+    [[nodiscard]] ValueResult<std::vector<unsigned char>> messageEntryGetVector(
+        rocksdb::Slice first_key_slice) const;
+    [[nodiscard]] ValueResult<std::vector<unsigned char>> checkpointGetVector(
+        rocksdb::Slice first_key_slice) const;
+
+   private:
+    ValueResult<std::vector<std::vector<unsigned char>>>
+    getVectorVectorUsingFamilyAndKey(rocksdb::ColumnFamilyHandle* family,
+                                     rocksdb::Slice first_key_slice,
+                                     size_t count) const;
+    ValueResult<std::vector<unsigned char>> getVectorUsingFamilyAndKey(
+        rocksdb::ColumnFamilyHandle* family,
+        rocksdb::Slice key_slice) const;
+    ValueResult<std::vector<uint256_t>> getUint256VectorUsingFamilyAndKey(
+        rocksdb::ColumnFamilyHandle* family,
+        rocksdb::Slice first_key_slice,
+        size_t count) const;
+    ValueResult<uint256_t> getUint256UsingFamilyAndKey(
+        rocksdb::ColumnFamilyHandle* family,
+        rocksdb::Slice key_slice) const;
+};
+
+#endif  // data_storage_readonlytransaction_hpp

packages/arb-avm-cpp/data_storage/include/data_storage/value/value.hpp
@@ -20,6 +20,7 @@
 #include <avm_values/value.hpp>
 #include <data_storage/value/valuecache.hpp>
 
+#include <data_storage/readonlytransaction.hpp>
 #include <map>
 #include <set>
 
@@ -30,21 +31,21 @@ class Transaction;
 template <typename T>
 struct DbResult;
 
-SaveResults saveValueImpl(Transaction& transaction,
+SaveResults saveValueImpl(ReadWriteTransaction& tx,
                           const value& val,
                           std::map<uint64_t, uint64_t>& segment_counts);
-DeleteResults deleteValueImpl(Transaction& transaction,
+DeleteResults deleteValueImpl(ReadWriteTransaction& tx,
                               const uint256_t& value_hash,
                               std::map<uint64_t, uint64_t>& segment_counts);
-DbResult<value> getValueImpl(const Transaction& transaction,
-                             uint256_t value_hash,
+DbResult<value> getValueImpl(const ReadOnlyTransaction& tx,
+                             const uint256_t value_hash,
                              std::set<uint64_t>& segment_ids,
                              ValueCache& value_cache);
 
-DbResult<value> getValue(const Transaction& transaction,
-                         uint256_t value_hash,
+DbResult<value> getValue(const ReadOnlyTransaction& tx,
+                         const uint256_t value_hash,
                          ValueCache& value_cache);
-SaveResults saveValue(Transaction& transaction, const value& val);
-DeleteResults deleteValue(Transaction& transaction, uint256_t value_hash);
+SaveResults saveValue(ReadWriteTransaction& tx, const value& val);
+DeleteResults deleteValue(ReadWriteTransaction& tx, uint256_t value_hash);
 
 #endif /* value_hpp */

OffchainLabs/token-bridge-contracts

packages/arb-avm-cpp/data_storage/include/data_storage/readonlytransaction.hpp
@@ -1,0 +1,116 @@
+/*
+ * Copyright 2021, Offchain Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef data_storage_readonlytransaction_hpp
+#define data_storage_readonlytransaction_hpp
+
+#include <data_storage/datastorage.hpp>
+
+class ReadWriteTransaction;
+
+class ReadOnlyTransaction {
+   private:
+    std::unique_ptr<Transaction> transaction{};
+    rocksdb::ReadOptions read_options{};
+
+    friend ReadWriteTransaction;
+
+   public:
+    explicit ReadOnlyTransaction(std::shared_ptr<DataStorage> store);
+    ~ReadOnlyTransaction();
+
+    static std::unique_ptr<ReadOnlyTransaction> makeReadOnlyTransaction(
+        std::shared_ptr<DataStorage> store);
+
+    void enterReadSnapshot();
+    void exitReadSnapshot();
+
+    rocksdb::Status defaultGet(const rocksdb::Slice& key,
+                               std::string* value) const;
+    rocksdb::Status stateGet(const rocksdb::Slice& key,
+                             std::string* value) const;
+    rocksdb::Status checkpointGet(const rocksdb::Slice& key,
+                                  std::string* value) const;
+    rocksdb::Status messageEntryGet(const rocksdb::Slice& key,
+                                    std::string* value) const;
+    rocksdb::Status logGet(const rocksdb::Slice& key, std::string* value) const;
+    rocksdb::Status sendGet(const rocksdb::Slice& key,
+                            std::string* value) const;
+    rocksdb::Status sideloadGet(const rocksdb::Slice& key,
+                                std::string* value) const;
+    [[nodiscard]] std::unique_ptr<rocksdb::Iterator> defaultGetIterator() const;
+    [[nodiscard]] std::unique_ptr<rocksdb::Iterator> stateGetIterator() const;
+    [[nodiscard]] std::unique_ptr<rocksdb::Iterator> checkpointGetIterator()
+        const;
+    [[nodiscard]] std::unique_ptr<rocksdb::Iterator> messageEntryGetIterator()
+        const;
+    [[nodiscard]] std::unique_ptr<rocksdb::Iterator> logGetIterator() const;
+    [[nodiscard]] std::unique_ptr<rocksdb::Iterator> sendGetIterator() const;
+    [[nodiscard]] std::unique_ptr<rocksdb::Iterator> sideloadGetIterator()
+        const;
+
+    [[nodiscard]] ValueResult<uint256_t> defaultGetUint256(
+        rocksdb::Slice key_slice) const;
+    [[nodiscard]] ValueResult<uint256_t> stateGetUint256(
+        rocksdb::Slice key_slice) const;
+    [[nodiscard]] ValueResult<uint256_t> checkpointGetUint256(
+        rocksdb::Slice key_slice) const;
+    [[nodiscard]] ValueResult<uint256_t> messageEntryGetUint256(
+        rocksdb::Slice key_slice) const;
+    [[nodiscard]] ValueResult<uint256_t> logGetUint256(
+        rocksdb::Slice key_slice) const;
+    [[nodiscard]] ValueResult<uint256_t> sendGetUint256(
+        rocksdb::Slice key_slice) const;
+    [[nodiscard]] ValueResult<uint256_t> sideloadGetUint256(
+        rocksdb::Slice key_slice) const;
+    [[nodiscard]] ValueResult<std::vector<uint256_t>> logGetUint256Vector(
+        rocksdb::Slice first_key_slice,
+        size_t count) const;
+    [[nodiscard]] ValueResult<std::vector<std::vector<unsigned char>>>
+    messageEntryGetVectorVector(rocksdb::Slice first_key_slice,
+                                size_t count) const;
+    [[nodiscard]] ValueResult<std::vector<std::vector<unsigned char>>>
+    sendGetVectorVector(rocksdb::Slice first_key_slice, size_t count) const;
+    [[nodiscard]] ValueResult<std::vector<unsigned char>> messageEntryGetVector(
+        rocksdb::Slice first_key_slice) const;
+    [[nodiscard]] ValueResult<std::vector<unsigned char>> checkpointGetVector(
+        rocksdb::Slice first_key_slice) const;
+
+    rocksdb::Status aggregatorGet(const rocksdb::Slice& key,
+                                  std::string* value) const;
+    [[nodiscard]] std::unique_ptr<rocksdb::Iterator> aggregatorGetIterator()
+        const;
+    [[nodiscard]] ValueResult<uint256_t> aggregatorGetUint256(
+        rocksdb::Slice key_slice) const;
+
+   private:
+    ValueResult<std::vector<std::vector<unsigned char>>>
+    getVectorVectorUsingFamilyAndKey(rocksdb::ColumnFamilyHandle* family,
+                                     rocksdb::Slice first_key_slice,
+                                     size_t count) const;
+    ValueResult<std::vector<unsigned char>> getVectorUsingFamilyAndKey(
+        rocksdb::ColumnFamilyHandle* family,
+        rocksdb::Slice key_slice) const;
+    ValueResult<std::vector<uint256_t>> getUint256VectorUsingFamilyAndKey(
+        rocksdb::ColumnFamilyHandle* family,
+        rocksdb::Slice first_key_slice,
+        size_t count) const;
+    ValueResult<uint256_t> getUint256UsingFamilyAndKey(
+        rocksdb::ColumnFamilyHandle* family,
+        rocksdb::Slice key_slice) const;
+};
+
+#endif  // data_storage_readonlytransaction_hpp

packages/arb-avm-cpp/data_storage/include/data_storage/value/value.hpp
@@ -21,6 +21,7 @@
 #include <data_storage/storageresultfwd.hpp>
 #include <data_storage/value/valuecache.hpp>
 
+#include <data_storage/readonlytransaction.hpp>
 #include <map>
 #include <set>
 
@@ -28,21 +29,21 @@ struct DeleteResults;
 struct SaveResults;
 class Transaction;
 
-SaveResults saveValueImpl(Transaction& transaction,
+SaveResults saveValueImpl(ReadWriteTransaction& transaction,
                           const value& val,
                           std::map<uint64_t, uint64_t>& segment_counts);
-DeleteResults deleteValueImpl(Transaction& transaction,
+DeleteResults deleteValueImpl(ReadWriteTransaction& tx,
                               const uint256_t& value_hash,
                               std::map<uint64_t, uint64_t>& segment_counts);
-DbResult<value> getValueImpl(const Transaction& transaction,
-                             uint256_t value_hash,
+DbResult<value> getValueImpl(const ReadOnlyTransaction& tx,
+                             const uint256_t value_hash,
                              std::set<uint64_t>& segment_ids,
                              ValueCache& value_cache);
 
-DbResult<value> getValue(const Transaction& transaction,
-                         uint256_t value_hash,
+DbResult<value> getValue(const ReadOnlyTransaction& tx,
+                         const uint256_t value_hash,
                          ValueCache& value_cache);
-SaveResults saveValue(Transaction& transaction, const value& val);
-DeleteResults deleteValue(Transaction& transaction, uint256_t value_hash);
+SaveResults saveValue(ReadWriteTransaction& tx, const value& val);
+DeleteResults deleteValue(ReadWriteTransaction& tx, uint256_t value_hash);
 
 #endif /* value_hpp */