d618bec5dbca20aff3ce6559b6f5ee996cb58a3f
gzeon <95478735+gzeoneth@users.noreply.github.com>
2023-02-01 00:02:53 +0900

    fix: wrong exitNum in L2WethGateway

--- contracts/tokenbridge/ethereum/gateway/L1WethGateway.sol
+++ contracts/tokenbridge/ethereum/gateway/L1WethGateway.sol
@@ -43,10 +43,6 @@ contract L1WethGateway is L1ArbitrumExtendedGateway {
         require(_l2Weth != address(0), "INVALID_L2WETH");
         l1Weth = _l1Weth;
         l2Weth = _l2Weth;
-        // we skip the first 2 exit numbers becuase a previous version of this contract had exitNum
-        // stuck at 1 which we have to prevent those exit being traded by reverting with INVALID_EXIT_NUM
-        // starting exitNum at 2 will prevent any new deployment triggering that revert
-        _exitNum = 2;
     }
 
     function createOutboundTxCustomRefund(
