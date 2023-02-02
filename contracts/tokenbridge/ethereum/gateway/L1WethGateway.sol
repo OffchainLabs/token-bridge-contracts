dce646c6890ab9c7c91c7d2ec70b4d83fa359a76
gzeon <95478735+gzeoneth@users.noreply.github.com>
2023-02-02 20:02:34 +0900

    docs: update natspec

--- contracts/tokenbridge/ethereum/gateway/L1WethGateway.sol
+++ contracts/tokenbridge/ethereum/gateway/L1WethGateway.sol
@@ -110,7 +110,7 @@ contract L1WethGateway is L1ArbitrumExtendedGateway {
     }
 
     /**
-     * @notice  this will revert with exitNum <= 1 to mitigate a known issue causing multiple exits with exitNum 0 and 1
+     * @notice  Temporary disable the ability to trade exits
      */
     function setRedirectedExit(
         uint256 _exitNum,
