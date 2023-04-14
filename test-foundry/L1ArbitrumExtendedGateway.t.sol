5b0d2d56ec236fbd89655dbf45970498333e2485
Goran Vladika <goran.vladika@gmail.com>
2023-04-14 09:41:06 +0200

    Add test cases for transferExitAndCall

--- test-foundry/L1ArbitrumExtendedGateway.t.sol
+++ test-foundry/L1ArbitrumExtendedGateway.t.sol
@@ -34,4 +34,88 @@ abstract contract L1ArbitrumExtendedGatewayTest is Test {
         assertEq(newTo, address(0), "Invalid _newTo");
         assertEq(newData.length, 0, "Invalid _newData");
     }
+
+    function test_transferExitAndCall_EmptyData(
+        uint256 exitNum,
+        address initialDestination,
+        address newDestination
+    ) public {
+        bytes memory newData;
+        bytes memory data;
+
+        // check event
+        vm.expectEmit(true, true, true, true);
+        emit WithdrawRedirected(initialDestination, newDestination, exitNum, newData, data, false);
+
+        // do it
+        vm.prank(initialDestination);
+        L1ArbitrumExtendedGateway(address(l1Gateway)).transferExitAndCall(
+            exitNum,
+            initialDestination,
+            newDestination,
+            newData,
+            data
+        );
+
+        // check exit data is properly updated
+        bytes32 exitId = keccak256(abi.encode(exitNum, initialDestination));
+        (bool isExit, address exitTo, bytes memory exitData) = L1ArbitrumExtendedGateway(
+            address(l1Gateway)
+        ).redirectedExits(exitId);
+        assertEq(isExit, true, "Invalid isExit");
+        assertEq(exitTo, newDestination, "Invalid exitTo");
+        assertEq(exitData.length, 0, "Invalid exitData");
+    }
+
+    function test_transferExitAndCall_revert_NotExpectedSender() public {
+        address nonSender = address(800);
+        vm.expectRevert("NOT_EXPECTED_SENDER");
+        L1ArbitrumExtendedGateway(address(l1Gateway)).transferExitAndCall(
+            4,
+            nonSender,
+            address(2),
+            "",
+            ""
+        );
+    }
+
+    function test_transferExitAndCall_revert_NoDataAllowed() public {
+        bytes memory nonEmptyData = bytes("abc");
+        vm.prank(address(1));
+        vm.expectRevert("NO_DATA_ALLOWED");
+        L1ArbitrumExtendedGateway(address(l1Gateway)).transferExitAndCall(
+            4,
+            address(1),
+            address(2),
+            nonEmptyData,
+            ""
+        );
+    }
+
+    function test_transferExitAndCall_revert_ToNotContract(address initialDestination) public {
+        bytes memory data = abi.encode("execute()");
+        address nonContractNewDestination = address(15);
+
+        vm.prank(initialDestination);
+        vm.expectRevert("TO_NOT_CONTRACT");
+        L1ArbitrumExtendedGateway(address(l1Gateway)).transferExitAndCall(
+            4,
+            initialDestination,
+            nonContractNewDestination,
+            "",
+            data
+        );
+    }
+
+    /////
+    /// Event declarations
+    /////
+    event WithdrawRedirected(
+        address indexed from,
+        address indexed to,
+        uint256 indexed exitNum,
+        bytes newData,
+        bytes data,
+        bool madeExternalCall
+    );
 }
