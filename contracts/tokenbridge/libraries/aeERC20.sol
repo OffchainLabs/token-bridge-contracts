eaf7dcbcef10cca4f9e680282953e694dfd46d27
Goran Vladika <goran.vladika@gmail.com>
2023-04-24 13:57:24 +0200

    Remove unused import

--- contracts/tokenbridge/libraries/aeERC20.sol
+++ contracts/tokenbridge/libraries/aeERC20.sol
@@ -18,7 +18,6 @@
 
 pragma solidity ^0.8.0;
 
-// import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
 import "../libraries/draft-ERC20PermitUpgradeable.sol";
 import "./TransferAndCallToken.sol";
 
