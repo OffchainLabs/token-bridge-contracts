2e15a1c5e014e91bcff04d9d30b50332f5784fe7
Goran Vladika <goran.vladika@gmail.com>
2023-11-09 16:00:36 +0100

    Add more L2ERC20Gateway tests

--- test-foundry/L2ERC20Gateway.t.sol
+++ test-foundry/L2ERC20Gateway.t.sol
@@ -10,6 +10,7 @@ import {
     ClonableBeaconProxy
 } from "contracts/tokenbridge/libraries/ClonableBeaconProxy.sol";
 import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
+import {ITokenGateway} from "contracts/tokenbridge/libraries/gateway/ITokenGateway.sol";
 
 contract L2ERC20GatewayTest is Test {
     L2ERC20Gateway public l2Gateway;
@@ -31,12 +32,45 @@ contract L2ERC20GatewayTest is Test {
     }
 
     /* solhint-disable func-name-mixedcase */
+    function test_calculateL2TokenAddress() public {
+        address l1Token = makeAddr("l1Token");
+        assertEq(
+            l2Gateway.getUserSalt(l1Token), keccak256(abi.encode(l1Token)), "Invalid user salt"
+        );
+    }
 
     function test_cloneableProxyHash() public {
         assertEq(
             l2Gateway.cloneableProxyHash(),
             keccak256(type(ClonableBeaconProxy).creationCode),
             "Invalid proxy hash"
+        );
+    }
+
+    function test_getOutboundCalldata() public {
+        address token = makeAddr("token");
+        address from = makeAddr("from");
+        address to = makeAddr("to");
+        uint256 amount = 200;
+        bytes memory data = new bytes(340);
+
+        bytes memory expected = abi.encodeWithSelector(
+            ITokenGateway.finalizeInboundTransfer.selector,
+            token,
+            from,
+            to,
+            amount,
+            abi.encode(0, data)
+        );
+        bytes memory actual = l2Gateway.getOutboundCalldata(token, from, to, amount, data);
+
+        assertEq(actual, expected, "Invalid outbound calldata");
+    }
+
+    function test_getUserSalt() public {
+        address l1Token = makeAddr("l1Token");
+        assertEq(
+            l2Gateway.getUserSalt(l1Token), keccak256(abi.encode(l1Token)), "Invalid user salt"
         );
     }
 
