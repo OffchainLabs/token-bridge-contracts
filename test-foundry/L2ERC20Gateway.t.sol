d0bc654c3e6394e2abb0bc678ad211ee2c22ebcb
Goran Vladika <goran.vladika@gmail.com>
2023-11-13 11:01:52 +0100

    Add outboundTransfer tests

--- test-foundry/L2ERC20Gateway.t.sol
+++ test-foundry/L2ERC20Gateway.t.sol
@@ -11,10 +11,13 @@ import {
 } from "contracts/tokenbridge/libraries/ClonableBeaconProxy.sol";
 import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
 import {ITokenGateway} from "contracts/tokenbridge/libraries/gateway/ITokenGateway.sol";
+import {ArbSysMock} from "contracts/tokenbridge/test/ArbSysMock.sol";
 
 contract L2ERC20GatewayTest is Test {
     L2ERC20Gateway public l2Gateway;
+    ArbSysMock public arbSysMock = new ArbSysMock();
 
+    address public user = makeAddr("user");
     address public l2BeaconProxyFactory;
     address public router = makeAddr("router");
     address public l1Counterpart = makeAddr("l1Counterpart");
@@ -107,4 +110,141 @@ contract L2ERC20GatewayTest is Test {
         vm.expectRevert("ALREADY_INIT");
         L2ERC20Gateway(gateway).initialize(l1Counterpart, router, l2BeaconProxyFactory);
     }
+
+    function test_outboundTransfer() public {
+        address l1Token = makeAddr("l1Token");
+
+        // create and init standard l2Token
+        bytes32 salt = keccak256(abi.encode(l1Token));
+        vm.startPrank(address(l2Gateway));
+        address l2Token = BeaconProxyFactory(l2BeaconProxyFactory).createProxy(salt);
+        StandardArbERC20(l2Token).bridgeInit(
+            l1Token,
+            abi.encode(
+                abi.encode(bytes("Name")), abi.encode(bytes("Symbol")), abi.encode(uint256(18))
+            )
+        );
+        vm.stopPrank();
+
+        // mint token to user
+        deal(l2Token, user, 100 ether);
+
+        // withdrawal params
+        address to = makeAddr("to");
+        uint256 amount = 2400;
+        bytes memory data = new bytes(0);
+
+        // events
+        uint256 expectedId = 0;
+        bytes memory expectedData = l2Gateway.getOutboundCalldata(l1Token, user, to, amount, data);
+        vm.expectEmit(true, true, true, true);
+        emit TxToL1(user, l1Counterpart, expectedId, expectedData);
+
+        vm.expectEmit(true, true, true, true);
+        emit WithdrawalInitiated(l1Token, user, to, expectedId, 0, amount);
+
+        // withdraw
+        vm.etch(0x0000000000000000000000000000000000000064, address(arbSysMock).code);
+        vm.prank(user);
+        l2Gateway.outboundTransfer(l1Token, to, amount, 0, 0, data);
+    }
+
+    function test_outboundTransfer_4Args() public {
+        address l1Token = makeAddr("l1Token");
+
+        // create and init standard l2Token
+        bytes32 salt = keccak256(abi.encode(l1Token));
+        vm.startPrank(address(l2Gateway));
+        address l2Token = BeaconProxyFactory(l2BeaconProxyFactory).createProxy(salt);
+        StandardArbERC20(l2Token).bridgeInit(
+            l1Token,
+            abi.encode(
+                abi.encode(bytes("Name")), abi.encode(bytes("Symbol")), abi.encode(uint256(18))
+            )
+        );
+        vm.stopPrank();
+
+        // mint token to user
+        deal(l2Token, user, 100 ether);
+
+        // withdrawal params
+        address to = makeAddr("to");
+        uint256 amount = 2400;
+        bytes memory data = new bytes(0);
+
+        // events
+        uint256 expectedId = 0;
+        bytes memory expectedData = l2Gateway.getOutboundCalldata(l1Token, user, to, amount, data);
+        vm.expectEmit(true, true, true, true);
+        emit TxToL1(user, l1Counterpart, expectedId, expectedData);
+
+        vm.expectEmit(true, true, true, true);
+        emit WithdrawalInitiated(l1Token, user, to, expectedId, 0, amount);
+
+        // withdraw
+        vm.etch(0x0000000000000000000000000000000000000064, address(arbSysMock).code);
+        vm.prank(user);
+        l2Gateway.outboundTransfer(l1Token, to, amount, data);
+    }
+
+    function test_outboundTransfer_revert_ExtraDataDisabled() public {
+        vm.expectRevert("EXTRA_DATA_DISABLED");
+        bytes memory extraData = new bytes(0x1234);
+        l2Gateway.outboundTransfer(address(100), address(101), 200, 0, 0, extraData);
+    }
+
+    function test_outboundTransfer_revert_NoValue() public {
+        vm.expectRevert("NO_VALUE");
+        l2Gateway.outboundTransfer{value: 1 ether}(
+            address(100), address(101), 200, 0, 0, new bytes(0)
+        );
+    }
+
+    function test_outboundTransfer_revert_NotExpectedL1Token() public {
+        /// register l1Token
+        address l1Token = makeAddr("l1Token");
+        bytes32 salt = keccak256(abi.encode(l1Token));
+        vm.startPrank(address(l2Gateway));
+        address l2Token = BeaconProxyFactory(l2BeaconProxyFactory).createProxy(salt);
+        StandardArbERC20(l2Token).bridgeInit(
+            l1Token,
+            abi.encode(
+                abi.encode(bytes("Name")), abi.encode(bytes("Symbol")), abi.encode(uint256(18))
+            )
+        );
+        vm.stopPrank();
+
+        // mock invalid L1 token ref
+        address notOriginalL1Token = makeAddr("notOriginalL1Token");
+        vm.mockCall(
+            address(l2Token), abi.encodeWithSignature("l1Address()"), abi.encode(notOriginalL1Token)
+        );
+
+        vm.expectRevert("NOT_EXPECTED_L1_TOKEN");
+        l2Gateway.outboundTransfer(l1Token, address(101), 200, 0, 0, new bytes(0));
+    }
+
+    function test_outboundTransfer_revert_TokenNotDeployed() public {
+        address l1Token = makeAddr("someToken");
+        vm.expectRevert("TOKEN_NOT_DEPLOYED");
+        l2Gateway.outboundTransfer(l1Token, address(101), 200, 0, 0, new bytes(0));
+    }
+
+    ////
+    // Event declarations
+    ////
+    event DepositFinalized(
+        address indexed l1Token, address indexed _from, address indexed _to, uint256 _amount
+    );
+
+    event WithdrawalInitiated(
+        address l1Token,
+        address indexed _from,
+        address indexed _to,
+        uint256 indexed _l2ToL1Id,
+        uint256 _exitNum,
+        uint256 _amount
+    );
+
+    event TxToL1(address indexed _from, address indexed _to, uint256 indexed _id, bytes _data);
 }
