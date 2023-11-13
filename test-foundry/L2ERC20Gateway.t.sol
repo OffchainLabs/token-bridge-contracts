2481dcf628b7cf2be61b280d5148ba774a02c91c
Goran Vladika <goran.vladika@gmail.com>
2023-11-13 15:13:40 +0100

    Add finalizeInboundTransfer tests

--- test-foundry/L2ERC20Gateway.t.sol
+++ test-foundry/L2ERC20Gateway.t.sol
@@ -12,15 +12,21 @@ import {
 import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
 import {ITokenGateway} from "contracts/tokenbridge/libraries/gateway/ITokenGateway.sol";
 import {ArbSysMock} from "contracts/tokenbridge/test/ArbSysMock.sol";
+import {AddressAliasHelper} from "contracts/tokenbridge/libraries/AddressAliasHelper.sol";
 
 contract L2ERC20GatewayTest is Test {
     L2ERC20Gateway public l2Gateway;
     ArbSysMock public arbSysMock = new ArbSysMock();
 
-    address public user = makeAddr("user");
     address public l2BeaconProxyFactory;
     address public router = makeAddr("router");
     address public l1Counterpart = makeAddr("l1Counterpart");
+
+    // token transfer params
+    address public l1Token = makeAddr("l1Token");
+    address public receiver = makeAddr("to");
+    address public sender = makeAddr("from");
+    uint256 public amount = 2400;
 
     function setUp() public virtual {
         l2Gateway = new L2ERC20Gateway();
@@ -36,7 +42,6 @@ contract L2ERC20GatewayTest is Test {
 
     /* solhint-disable func-name-mixedcase */
     function test_calculateL2TokenAddress() public {
-        address l1Token = makeAddr("l1Token");
         assertEq(
             l2Gateway.getUserSalt(l1Token), keccak256(abi.encode(l1Token)), "Invalid user salt"
         );
@@ -50,28 +55,111 @@ contract L2ERC20GatewayTest is Test {
         );
     }
 
+    function test_finalizeInboundTransfer() public {
+        /// deposit params
+        bytes memory gatewayData = abi.encode(
+            abi.encode(bytes("Name")), abi.encode(bytes("Symbol")), abi.encode(uint256(18))
+        );
+        bytes memory callHookData = new bytes(0);
+
+        /// events
+        vm.expectEmit(true, true, true, true);
+        emit DepositFinalized(l1Token, sender, receiver, amount);
+
+        /// finalize deposit
+        vm.prank(AddressAliasHelper.applyL1ToL2Alias(l1Counterpart));
+        l2Gateway.finalizeInboundTransfer(
+            l1Token, sender, receiver, amount, abi.encode(gatewayData, callHookData)
+        );
+
+        /// check tokens have been minted to receiver
+        address expectedL2Address = l2Gateway.calculateL2TokenAddress(l1Token);
+        assertEq(
+            StandardArbERC20(expectedL2Address).balanceOf(receiver),
+            amount,
+            "Invalid receiver balance"
+        );
+    }
+
+    function test_finalizeInboundTransfer_WithCallHook() public {
+        /// deposit params
+        bytes memory gatewayData = abi.encode(
+            abi.encode(bytes("Name")), abi.encode(bytes("Symbol")), abi.encode(uint256(18))
+        );
+        bytes memory callHookData = new bytes(0x1234ab);
+
+        /// events
+        vm.expectEmit(true, true, true, true);
+        emit DepositFinalized(l1Token, sender, receiver, amount);
+
+        /// finalize deposit
+        vm.prank(AddressAliasHelper.applyL1ToL2Alias(l1Counterpart));
+        l2Gateway.finalizeInboundTransfer(
+            l1Token, sender, receiver, amount, abi.encode(gatewayData, callHookData)
+        );
+
+        /// check tokens have been minted to receiver
+        address expectedL2Address = l2Gateway.calculateL2TokenAddress(l1Token);
+        assertEq(
+            StandardArbERC20(expectedL2Address).balanceOf(receiver),
+            amount,
+            "Invalid receiver balance"
+        );
+    }
+
+    function test_finalizeInboundTransfer_ShouldHalt() public {
+        /// deposit params
+        bytes memory gatewayData = abi.encode(
+            abi.encode(bytes("Name")), abi.encode(bytes("Symbol")), abi.encode(uint256(18))
+        );
+        bytes memory callHookData = new bytes(0);
+
+        // mock incorrect address calculation
+        address notL2Token = makeAddr("notL2Token");
+        vm.mockCall(
+            address(l2BeaconProxyFactory),
+            abi.encodeWithSignature(
+                "calculateExpectedAddress(address,bytes32)",
+                address(l2Gateway),
+                l2Gateway.getUserSalt(l1Token)
+            ),
+            abi.encode(notL2Token)
+        );
+
+        // check that withdrawal is triggered occurs when deposit is halted
+        vm.expectEmit(true, true, true, true);
+        emit WithdrawalInitiated(l1Token, address(l2Gateway), sender, 0, 0, amount);
+
+        /// finalize deposit
+        vm.etch(0x0000000000000000000000000000000000000064, address(arbSysMock).code);
+        vm.prank(AddressAliasHelper.applyL1ToL2Alias(l1Counterpart));
+        l2Gateway.finalizeInboundTransfer(
+            l1Token, sender, receiver, amount, abi.encode(gatewayData, callHookData)
+        );
+
+        /// check L2 token hasn't been creted
+        address expectedL2Address = l2Gateway.calculateL2TokenAddress(l1Token);
+        assertEq(address(notL2Token).code.length, 0, "L2 token isn't supposed to be created");
+    }
+
     function test_getOutboundCalldata() public {
         address token = makeAddr("token");
-        address from = makeAddr("from");
-        address to = makeAddr("to");
-        uint256 amount = 200;
         bytes memory data = new bytes(340);
 
         bytes memory expected = abi.encodeWithSelector(
             ITokenGateway.finalizeInboundTransfer.selector,
             token,
-            from,
-            to,
+            sender,
+            receiver,
             amount,
             abi.encode(0, data)
         );
-        bytes memory actual = l2Gateway.getOutboundCalldata(token, from, to, amount, data);
+        bytes memory actual = l2Gateway.getOutboundCalldata(token, sender, receiver, amount, data);
 
         assertEq(actual, expected, "Invalid outbound calldata");
     }
 
     function test_getUserSalt() public {
-        address l1Token = makeAddr("l1Token");
         assertEq(
             l2Gateway.getUserSalt(l1Token), keccak256(abi.encode(l1Token)), "Invalid user salt"
         );
@@ -112,8 +200,6 @@ contract L2ERC20GatewayTest is Test {
     }
 
     function test_outboundTransfer() public {
-        address l1Token = makeAddr("l1Token");
-
         // create and init standard l2Token
         bytes32 salt = keccak256(abi.encode(l1Token));
         vm.startPrank(address(l2Gateway));
@@ -127,31 +213,28 @@ contract L2ERC20GatewayTest is Test {
         vm.stopPrank();
 
         // mint token to user
-        deal(l2Token, user, 100 ether);
+        deal(l2Token, sender, 100 ether);
 
         // withdrawal params
-        address to = makeAddr("to");
-        uint256 amount = 2400;
         bytes memory data = new bytes(0);
 
         // events
         uint256 expectedId = 0;
-        bytes memory expectedData = l2Gateway.getOutboundCalldata(l1Token, user, to, amount, data);
+        bytes memory expectedData =
+            l2Gateway.getOutboundCalldata(l1Token, sender, receiver, amount, data);
         vm.expectEmit(true, true, true, true);
-        emit TxToL1(user, l1Counterpart, expectedId, expectedData);
+        emit TxToL1(sender, l1Counterpart, expectedId, expectedData);
 
         vm.expectEmit(true, true, true, true);
-        emit WithdrawalInitiated(l1Token, user, to, expectedId, 0, amount);
+        emit WithdrawalInitiated(l1Token, sender, receiver, expectedId, 0, amount);
 
         // withdraw
         vm.etch(0x0000000000000000000000000000000000000064, address(arbSysMock).code);
-        vm.prank(user);
-        l2Gateway.outboundTransfer(l1Token, to, amount, 0, 0, data);
+        vm.prank(sender);
+        l2Gateway.outboundTransfer(l1Token, receiver, amount, 0, 0, data);
     }
 
     function test_outboundTransfer_4Args() public {
-        address l1Token = makeAddr("l1Token");
-
         // create and init standard l2Token
         bytes32 salt = keccak256(abi.encode(l1Token));
         vm.startPrank(address(l2Gateway));
@@ -165,26 +248,25 @@ contract L2ERC20GatewayTest is Test {
         vm.stopPrank();
 
         // mint token to user
-        deal(l2Token, user, 100 ether);
+        deal(l2Token, sender, 100 ether);
 
         // withdrawal params
-        address to = makeAddr("to");
-        uint256 amount = 2400;
         bytes memory data = new bytes(0);
 
         // events
         uint256 expectedId = 0;
-        bytes memory expectedData = l2Gateway.getOutboundCalldata(l1Token, user, to, amount, data);
+        bytes memory expectedData =
+            l2Gateway.getOutboundCalldata(l1Token, sender, receiver, amount, data);
         vm.expectEmit(true, true, true, true);
-        emit TxToL1(user, l1Counterpart, expectedId, expectedData);
+        emit TxToL1(sender, l1Counterpart, expectedId, expectedData);
 
         vm.expectEmit(true, true, true, true);
-        emit WithdrawalInitiated(l1Token, user, to, expectedId, 0, amount);
+        emit WithdrawalInitiated(l1Token, sender, receiver, expectedId, 0, amount);
 
         // withdraw
         vm.etch(0x0000000000000000000000000000000000000064, address(arbSysMock).code);
-        vm.prank(user);
-        l2Gateway.outboundTransfer(l1Token, to, amount, data);
+        vm.prank(sender);
+        l2Gateway.outboundTransfer(l1Token, receiver, amount, data);
     }
 
     function test_outboundTransfer_revert_ExtraDataDisabled() public {
@@ -202,7 +284,6 @@ contract L2ERC20GatewayTest is Test {
 
     function test_outboundTransfer_revert_NotExpectedL1Token() public {
         /// register l1Token
-        address l1Token = makeAddr("l1Token");
         bytes32 salt = keccak256(abi.encode(l1Token));
         vm.startPrank(address(l2Gateway));
         address l2Token = BeaconProxyFactory(l2BeaconProxyFactory).createProxy(salt);
@@ -225,26 +306,28 @@ contract L2ERC20GatewayTest is Test {
     }
 
     function test_outboundTransfer_revert_TokenNotDeployed() public {
-        address l1Token = makeAddr("someToken");
+        address token = makeAddr("someToken");
         vm.expectRevert("TOKEN_NOT_DEPLOYED");
-        l2Gateway.outboundTransfer(l1Token, address(101), 200, 0, 0, new bytes(0));
+        l2Gateway.outboundTransfer(token, address(101), 200, 0, 0, new bytes(0));
     }
 
     ////
     // Event declarations
     ////
     event DepositFinalized(
-        address indexed l1Token, address indexed _from, address indexed _to, uint256 _amount
+        address indexed l1Token, address indexed _from, address indexed _receiver, uint256 _amount
     );
 
     event WithdrawalInitiated(
         address l1Token,
         address indexed _from,
-        address indexed _to,
+        address indexed _receiver,
         uint256 indexed _l2ToL1Id,
         uint256 _exitNum,
         uint256 _amount
     );
 
-    event TxToL1(address indexed _from, address indexed _to, uint256 indexed _id, bytes _data);
+    event TxToL1(
+        address indexed _from, address indexed _receiver, uint256 indexed _id, bytes _data
+    );
 }
