f60ec8659884a6dc71b68512aed562e87418fe9a
Goran Vladika <goran.vladika@gmail.com>
2023-11-17 12:56:37 +0100

    Check constructor size and throw error if unexpected one

--- scripts/ci/constructorCheck.ts
+++ scripts/ci/constructorCheck.ts
@@ -1,14 +1,38 @@
 import hre from 'hardhat'
 
+/**
+ * Contracts defined here are the one for which cosntructor size check will be performed.
+ */
+export const CONTRACTS_TO_EXPECTED_CONSTRUCTOR_SIZE: Record<string, number> = {
+  L2AtomicTokenBridgeFactory: 32,
+  ArbMulticall2: 32,
+  L2GatewayRouter: 32,
+  L2ERC20Gateway: 32,
+  L2CustomGateway: 32,
+  L2WethGateway: 32,
+  aeWETH: 348,
+}
+
 main().then(() => console.log('Done.'))
 
 async function main() {
-  const contractName = 'L2GatewayRouter'
+  for (const [contractName, expectedLength] of Object.entries(
+    CONTRACTS_TO_EXPECTED_CONSTRUCTOR_SIZE
+  )) {
+    // Extracting the constructor prefix
+    const constructorBytecode = await _getConstructorBytecode(contractName)
+    const constructorBytecodeLength = _lengthInBytes(constructorBytecode)
 
-  // Extracting the constructor prefix
-  const constructorBytecode = await _getConstructorBytecode(contractName)
+    console.log(
+      `Constructor length of ${contractName} is ${constructorBytecodeLength} bytes, expected ${expectedLength} bytes.`
+    )
 
-  console.log('Constructor code:', constructorBytecode)
+    if (constructorBytecodeLength !== expectedLength) {
+      throw new Error(
+        `Constructor length of ${contractName} is ${constructorBytecodeLength} bytes, expected ${expectedLength} bytes.`
+      )
+    }
+  }
 }
 
 /**
@@ -26,4 +50,13 @@ async function _getConstructorBytecode(contractName: string): Promise<string> {
   // extract the constructor prefix
   const constructorPrefix = completeBytecode.replace(deployedBytecode, '')
   return constructorPrefix
+}
+
+/**
+ * Every byte in the constructor bytecode is represented by 2 characters in hex
+ * @param hex
+ * @returns
+ */
+function _lengthInBytes(hex: string): number {
+  return hex.length / 2
 }
