55d6f076ddc73c68558e19ff29a35323dfbdbfa7
Goran Vladika <goran.vladika@gmail.com>
2023-11-28 13:00:01 +0100

    Add aeWETH/UpgradeExecutor constructor size check

--- test-e2e/creationCodeTest.ts
+++ test-e2e/creationCodeTest.ts
@@ -12,10 +12,38 @@ import fs from 'fs'
 
 const LOCALHOST_L2_RPC = 'http://localhost:8547'
 
+const AE_WETH_EXPECTED_CONSTRUCTOR_SIZE = 348
+const UPGRADE_EXECUTOR_EXPECTED_CONSTRUCTOR_SIZE = 242
+
 let provider: JsonRpcProvider
 let creationCodeTester: CreationCodeTest
 let l1TokenBridgeCreator: L1AtomicTokenBridgeCreator
 
+/**
+ * This test ensures that the Solidity lib generates the same creation code as the
+ * compiler for the contracts which are deployed to the child chain.
+ *
+ * The reason why we perform constructor check is due to atomic token bridge creator
+ * implementation. Due to contract size limits, we deploy child chain templates to the
+ * parent chain. When token bridge is being created, parent chain creator will fetch the
+ * runtime bytecode of the templates and send it to the child chain via retryable tickets.
+ * Child chain factory will then prepend the empty-constructor bytecode to the runtime code
+ * and use resulting bytecode for deployment. That's why we need to ensure that those
+ * impacted contracts don't have any logic in their constructors, as that logic can't be
+ * executed when deploying to the child chain.
+ *
+ * All impacted contracts have 32 bytes of constructor bytecode which look like this:
+ *   608060405234801561001057600080fd5b50615e7c806100206000396000f3fe
+ * This constructor checks that there's no callvalue, copies the contract code to memory
+ * and returns it. The only place where constructor bytecode differs between contracts
+ * is in 61xxxx80 where xxxx is the length of the contract's bytecode.
+ *
+ * Exception are aeWETH and UpgradeExecutor contracts. Their constructors are not empty as they
+ * contain logic to set the logic contract to the initialized state. In our system we need to
+ * perform this initialization by chaild chain factory. It is important though that constructor
+ * for these contracts never changes. That's why we check the constructor size matches the
+ * expected hardcoded size.
+ */
 describe('creationCodeTest', () => {
   before(async function () {
     /// get default deployer params in local test env
@@ -84,6 +112,24 @@ describe('creationCodeTest', () => {
       )
     )
   })
+
+  it('aeWETH constructor has expected size', async function () {
+    const constructorBytecode = await _getConstructorBytecode('aeWETH')
+    const constructorBytecodeLength = _lengthInBytes(constructorBytecode)
+
+    expect(constructorBytecodeLength).to.be.eq(
+      AE_WETH_EXPECTED_CONSTRUCTOR_SIZE
+    )
+  })
+
+  it('UpgradeExecutor constructor has expected size', async function () {
+    const constructorBytecode = await _getConstructorBytecode('UpgradeExecutor')
+    const constructorBytecodeLength = _lengthInBytes(constructorBytecode)
+
+    expect(constructorBytecodeLength).to.be.eq(
+      UPGRADE_EXECUTOR_EXPECTED_CONSTRUCTOR_SIZE
+    )
+  })
 })
 
 async function _getCompilerGeneratedCreationCode(
@@ -118,4 +164,35 @@ async function _getTokenBridgeCreator(
     data['l1TokenBridgeCreator'],
     provider
   )
+}
+
+/**
+ * Get constructor bytecode as a difference between creation and deployed bytecode
+ * @param contractName
+ * @returns
+ */
+async function _getConstructorBytecode(contractName: string): Promise<string> {
+  const artifact = await hre.artifacts.readArtifact(contractName)
+
+  // remove '0x'
+  const creationCode = artifact.bytecode.substring(2)
+  const runtimeCode = artifact.deployedBytecode.substring(2)
+
+  if (!creationCode.includes(runtimeCode)) {
+    throw new Error(
+      `Error while extracting constructor bytecode for contract ${contractName}.`
+    )
+  }
+
+  // extract the constructor code
+  return creationCode.replace(runtimeCode, '')
+}
+
+/**
+ * Every byte in the constructor bytecode is represented by 2 characters in hex
+ * @param hex
+ * @returns
+ */
+function _lengthInBytes(hex: string): number {
+  return hex.length / 2
 }
