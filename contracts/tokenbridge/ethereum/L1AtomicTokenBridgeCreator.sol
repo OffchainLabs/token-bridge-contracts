a2276ef14222a9ccccaf8dcbfdc03d9465337a5d
gzeon <im@gzeon.dev>
2023-12-01 00:01:51 +0900

    refactor: _computeAddressAtNonce0

--- contracts/tokenbridge/ethereum/L1AtomicTokenBridgeCreator.sol
+++ contracts/tokenbridge/ethereum/L1AtomicTokenBridgeCreator.sol
@@ -147,7 +147,7 @@ contract L1AtomicTokenBridgeCreator is Initializable, OwnableUpgradeable {
         retryableSender.initialize();
 
         canonicalL2FactoryAddress =
-            _computeAddress(AddressAliasHelper.applyL1ToL2Alias(address(this)), 0);
+            _computeAddressAtNonce0(AddressAliasHelper.applyL1ToL2Alias(address(this)));
     }
 
     /**
@@ -535,22 +535,14 @@ contract L1AtomicTokenBridgeCreator is Initializable, OwnableUpgradeable {
      *
      * @return computed address
      */
-    function _computeAddress(address origin, uint256 nonce) internal pure returns (address) {
-        bytes memory data;
-        if (nonce == 0x00) {
-            data = abi.encodePacked(bytes1(0xd6), bytes1(0x94), origin, bytes1(0x80));
-        } else if (nonce <= 0x7f) {
-            data = abi.encodePacked(bytes1(0xd6), bytes1(0x94), origin, uint8(nonce));
-        } else if (nonce <= 0xff) {
-            data = abi.encodePacked(bytes1(0xd7), bytes1(0x94), origin, bytes1(0x81), uint8(nonce));
-        } else if (nonce <= 0xffff) {
-            data = abi.encodePacked(bytes1(0xd8), bytes1(0x94), origin, bytes1(0x82), uint16(nonce));
-        } else if (nonce <= 0xffffff) {
-            data = abi.encodePacked(bytes1(0xd9), bytes1(0x94), origin, bytes1(0x83), uint24(nonce));
-        } else {
-            data = abi.encodePacked(bytes1(0xda), bytes1(0x94), origin, bytes1(0x84), uint32(nonce));
-        }
-        return address(uint160(uint256(keccak256(data))));
+    function _computeAddressAtNonce0(address origin) internal pure returns (address) {
+        return address(
+            uint160(
+                uint256(
+                    keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), origin, bytes1(0x80)))
+                )
+            )
+        );
     }
 
     /**
