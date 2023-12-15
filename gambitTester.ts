b11dda4966e8175b66819eb86ef0d92bc668c493
Goran Vladika <goran.vladika@gmail.com>
2023-12-15 17:50:09 +0100

    Test mutants in separate folders

--- gambitTester.ts
+++ gambitTester.ts
@@ -1,11 +1,20 @@
 import { execSync } from 'child_process'
-import { existsSync, readdirSync, copyFileSync, unlinkSync } from 'fs'
+import { copyFileSync, unlinkSync } from 'fs'
 import * as path from 'path'
 import * as fs from 'fs'
+import * as fsExtra from 'fs-extra'
 
 const contractPath = 'contracts/tokenbridge/ethereum/gateway/L1ERC20Gateway.sol'
 const gambitDir = 'gambit_out/'
 const mutantsListFile = 'gambit_out/gambit_results.json'
+const testItems = [
+  'contracts',
+  'lib',
+  'foundry.toml',
+  'node_modules',
+  'remappings.txt',
+  'test-foundry',
+]
 
 interface Mutant {
   description: string
@@ -47,21 +56,30 @@ async function runMutationTesting() {
   })
 }
 
-function testMutant(mutant: Mutant) {
-  console.log(`Testing mutant: ${mutant.id}`)
+async function testMutant(mutant: Mutant) {
+  const testDirectory = path.join(__dirname, `test_mutant`, mutant.id)
+
+  console.log(`Testing mutant: ${mutant.id} in ${testDirectory}`)
+
+  await fsExtra.ensureDir(testDirectory)
+  for (const item of testItems) {
+    const sourcePath = path.join(__dirname, item)
+    const destPath = path.join(testDirectory, item)
+    await fsExtra.copy(sourcePath, destPath)
+  }
 
   // Replace original file with mutant
-  copyFileSync(path.join(gambitDir, mutant.name), mutant.original)
+  copyFileSync(
+    path.join(gambitDir, mutant.name),
+    path.join(testDirectory, mutant.original)
+  )
 
   // Re-build and test
   try {
-    execSync('forge build')
-    execSync('forge test')
+    execSync(`forge build --root ${testDirectory}`)
+    execSync(`forge test --root ${testDirectory}`)
     testResults.push({ mutant: mutant.id, status: 'Survived' })
   } catch (error) {
     testResults.push({ mutant: mutant.id, status: 'Killed' })
   }
-
-  // Restore original file
-  unlinkSync(contractPath)
 }
