33ba42bf301647e75692ac133a38c9b68b4d07eb
Goran Vladika <goran.vladika@gmail.com>
2023-12-18 10:06:00 +0100

    Paralellize tests

--- gambitTester.ts
+++ gambitTester.ts
@@ -1,5 +1,7 @@
-import { execSync } from 'child_process'
-import { copyFileSync, unlinkSync } from 'fs'
+import { exec } from 'child_process'
+import { copyFileSync } from 'fs'
+import { promisify } from 'util'
+import os from 'os'
 import * as path from 'path'
 import * as fs from 'fs'
 import * as fsExtra from 'fs-extra'
@@ -15,6 +17,8 @@ const testItems = [
   'remappings.txt',
   'test-foundry',
 ]
+const MAX_TASKS = os.cpus().length
+const execAsync = promisify(exec)
 
 interface Mutant {
   description: string
@@ -29,37 +33,40 @@ interface TestResult {
   status: string
 }
 
-const testResults: TestResult[] = []
-
 runMutationTesting().catch(error => {
   console.error('Error during mutation testing:', error)
 })
 
 async function runMutationTesting() {
   // generate mutants
-  execSync(
-    `gambit mutate -n 3 --solc_remappings "@openzeppelin=node_modules/@openzeppelin" "@arbitrum=node_modules/@arbitrum" -f ${contractPath}`
+  await execAsync(
+    `gambit mutate --solc_remappings "@openzeppelin=node_modules/@openzeppelin" "@arbitrum=node_modules/@arbitrum" -f ${contractPath}`
   )
 
-  // read mutants
-  const mutants: Mutant[] = JSON.parse(fs.readFileSync(mutantsListFile, 'utf8'))
-
   // test mutants
-  for (const mutant of mutants) {
-    testMutant(mutant)
+  const mutants: Mutant[] = JSON.parse(fs.readFileSync(mutantsListFile, 'utf8'))
+  const results: TestResult[] = []
+  for (let i = 0; i < mutants.length; i += MAX_TASKS) {
+    const currentBatch = mutants.slice(i, i + MAX_TASKS)
+    const batchPromises = currentBatch.map(mutant => {
+      return testMutant(mutant)
+    })
+
+    // Wait for the current batch of tests to complete
+    const batchResults = await Promise.all(batchPromises)
+    console.log('Batch results:', batchResults)
+    results.push(...batchResults)
   }
 
   // Print summary
   console.log('Mutation Testing Results:')
-  testResults.forEach(result => {
+  results.forEach(result => {
     console.log(`${result.mutant}: ${result.status}`)
   })
 }
 
-async function testMutant(mutant: Mutant) {
+async function testMutant(mutant: Mutant): Promise<TestResult> {
   const testDirectory = path.join(__dirname, `test_mutant`, mutant.id)
-
-  console.log(`Testing mutant: ${mutant.id} in ${testDirectory}`)
 
   await fsExtra.ensureDir(testDirectory)
   for (const item of testItems) {
@@ -76,10 +83,11 @@ async function testMutant(mutant: Mutant) {
 
   // Re-build and test
   try {
-    execSync(`forge build --root ${testDirectory}`)
-    execSync(`forge test --root ${testDirectory}`)
-    testResults.push({ mutant: mutant.id, status: 'Survived' })
+    console.log(`Building and testing mutant ${mutant.id} in ${testDirectory}`)
+    await execAsync(`forge build --root ${testDirectory}`)
+    await execAsync(`forge test --root ${testDirectory}`)
+    return { mutant: mutant.id, status: 'Survived' }
   } catch (error) {
-    testResults.push({ mutant: mutant.id, status: 'Killed' })
+    return { mutant: mutant.id, status: 'Killed' }
   }
 }
