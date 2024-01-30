ef869d3c6c5b5bf9a6434bc3ba0b94e885935c3e
Henry <11198460+godzillaba@users.noreply.github.com>
2024-01-29 20:08:14 -0500

    ...

--- scripts/local-deployment/deployCreatorAndCreateTokenBridge.ts
+++ scripts/local-deployment/deployCreatorAndCreateTokenBridge.ts
@@ -154,10 +154,7 @@ export const setupTokenBridgeInLocalEnv = async () => {
 
   // register weth gateway if not using custom fee token
   if (
-    (await getNativeToken(
-      coreL2Network.ethBridge.bridge,
-      parentDeployer.provider!
-    )) !== ethers.constants.AddressZero
+    l1Deployment.wethGateway === ethers.constants.AddressZero
   ) {
     const executorKey =
       '0xcb5790da63720727af975f42c79f69918580209889225fa7128c92402a6d3a65' // todo: get from env
