bc1c2a2a375be86d04b0b80c5d3c26d11bd50b79
Goran Vladika <goran.vladika@gmail.com>
2024-05-21 14:22:02 +0200

    Add pauseDeposits tests

--- test-foundry/L1USDCCustomGateway.t.sol
+++ test-foundry/L1USDCCustomGateway.t.sol
@@ -3,12 +3,14 @@
 pragma solidity ^0.8.0;
 
 import "./L1ArbitrumExtendedGateway.t.sol";
-import {L1USDCCustomGateway} from "contracts/tokenbridge/ethereum/gateway/L1USDCCustomGateway.sol";
+import {
+    L1USDCCustomGateway,
+    L2USDCCustomGateway
+} from "contracts/tokenbridge/ethereum/gateway/L1USDCCustomGateway.sol";
 
 contract L1USDCCustomGatewayTest is L1ArbitrumExtendedGatewayTest {
-    // gateway params
+    L1USDCCustomGateway usdcGateway;
     address public owner = makeAddr("gw-owner");
-
     address public L1_USDC = makeAddr("L1_USDC");
     address public L2_USDC = makeAddr("L2_USDC");
 
@@ -16,14 +18,13 @@ contract L1USDCCustomGatewayTest is L1ArbitrumExtendedGatewayTest {
         inbox = address(new InboxMock());
 
         l1Gateway = new L1USDCCustomGateway();
-        L1USDCCustomGateway(payable(address(l1Gateway))).initialize(
-            l2Gateway, router, inbox, L1_USDC, L2_USDC, owner
-        );
+        usdcGateway = L1USDCCustomGateway(payable(address(l1Gateway)));
+        usdcGateway.initialize(l2Gateway, router, inbox, L1_USDC, L2_USDC, owner);
 
-        maxSubmissionCost = 20;
+        maxSubmissionCost = 4000;
         retryableCost = maxSubmissionCost + gasPriceBid * maxGas;
 
-        vm.deal(router, 100 ether);
+        vm.deal(owner, 100 ether);
     }
 
     /* solhint-disable func-name-mixedcase */
@@ -46,9 +47,73 @@ contract L1USDCCustomGatewayTest is L1ArbitrumExtendedGatewayTest {
         assertEq(gateway.l1USDC(), L1_USDC, "Invalid L1_USDC");
         assertEq(gateway.l2USDC(), L2_USDC, "Invalid L2_USDC");
         assertEq(gateway.owner(), owner, "Invalid owner");
+        assertEq(gateway.depositsPaused(), false, "Invalid depositPaused");
     }
 
     function test_finalizeInboundTransfer() public override {
         // TODO
     }
+
+    function test_pauseDeposits() public {
+        assertEq(usdcGateway.depositsPaused(), false, "Invalid depositPaused");
+
+        vm.expectEmit(true, true, true, true);
+        emit DepositsPaused();
+
+        vm.expectEmit(true, true, true, true);
+        emit TicketData(maxSubmissionCost);
+
+        vm.expectEmit(true, true, true, true);
+        emit RefundAddresses(creditBackAddress, creditBackAddress);
+
+        vm.expectEmit(true, true, true, true);
+        emit InboxRetryableTicket(
+            address(usdcGateway),
+            l2Gateway,
+            0,
+            maxGas,
+            abi.encodeWithSelector(L2USDCCustomGateway.pauseWithdrawals.selector)
+        );
+
+        vm.prank(owner);
+        usdcGateway.pauseDeposits{value: retryableCost}(
+            maxGas, gasPriceBid, maxSubmissionCost, creditBackAddress
+        );
+
+        assertEq(usdcGateway.depositsPaused(), true, "Invalid depositPaused");
+    }
+
+    function test_pauseDeposits_revert_NotOwner() public {
+        vm.expectRevert("Ownable: caller is not the owner");
+        usdcGateway.pauseDeposits{value: retryableCost}(
+            maxGas, gasPriceBid, maxSubmissionCost, creditBackAddress
+        );
+    }
+
+    function test_pauseDeposits_revert_DepositsAlreadyPaused() public {
+        vm.prank(owner);
+        usdcGateway.pauseDeposits{value: retryableCost}(
+            maxGas, gasPriceBid, maxSubmissionCost, creditBackAddress
+        );
+
+        vm.prank(owner);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                L1USDCCustomGateway.L1USDCCustomGateway_DepositsAlreadyPaused.selector
+            )
+        );
+        usdcGateway.pauseDeposits{value: retryableCost}(
+            maxGas, gasPriceBid, maxSubmissionCost, creditBackAddress
+        );
+    }
+
+    ////
+    // Event declarations
+    ////
+    event DepositsPaused();
+    event GatewayUsdcBurned(uint256 amount);
+
+    event TicketData(uint256 maxSubmissionCost);
+    event RefundAddresses(address excessFeeRefundAddress, address callValueRefundAddress);
+    event InboxRetryableTicket(address from, address to, uint256 value, uint256 maxGas, bytes data);
 }
