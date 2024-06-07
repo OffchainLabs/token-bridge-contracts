98d711e6b8f706b7013392a0ca11506fa53ed626
Goran Vladika <goran.vladika@gmail.com>
2024-06-07 11:11:40 +0200

    Add initializtion of usdc token

--- test-foundry/L2USDCCustomGateway.t.sol
+++ test-foundry/L2USDCCustomGateway.t.sol
@@ -278,21 +278,3 @@ contract L2USDCCustomGatewayTest is L2ArbitrumGatewayTest {
     ////
     event WithdrawalsPaused();
 }
-
-interface FiatTokenArbitrumOrbitV2_2 {
-    function initialize(
-        string memory tokenName,
-        string memory tokenSymbol,
-        string memory tokenCurrency,
-        uint8 tokenDecimals,
-        address newMasterMinter,
-        address newPauser,
-        address newBlacklister,
-        address newOwner
-    ) external;
-    function initializeV2(string calldata newName) external;
-    function initializeV2_1(address lostAndFound) external;
-    function initializeV2_2(address[] calldata accountsToBlacklist, string calldata newSymbol)
-        external;
-    function initializeArbitrumOrbit(address _l2Gateway, address _l1Token) external;
-}
