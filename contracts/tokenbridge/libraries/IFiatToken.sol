9dab9f462192990170ad64136e9d503dddbdf39e
Goran Vladika <goran.vladika@gmail.com>
2024-06-19 14:40:09 +0200

    Use FiatTokenV2_2

--- contracts/tokenbridge/libraries/IFiatToken.sol
+++ contracts/tokenbridge/libraries/IFiatToken.sol
@@ -10,6 +10,21 @@ pragma solidity >0.6.0 <0.9.0;
  *      This interface is used in the L1USDCGateway, L1OrbitUSDCGateway and L2USDCGateway contracts.
  */
 interface IFiatToken {
+    function initialize(
+        string memory tokenName,
+        string memory tokenSymbol,
+        string memory tokenCurrency,
+        uint8 tokenDecimals,
+        address newMasterMinter,
+        address newPauser,
+        address newBlacklister,
+        address newOwner
+    ) external;
+    function initializeV2(string calldata newName) external;
+    function initializeV2_1(address lostAndFound) external;
+    function initializeV2_2(address[] calldata accountsToBlacklist, string calldata newSymbol)
+        external;
+    function configureMinter(address minter, uint256 minterAllowedAmount) external;
     function burn(uint256 _amount) external;
     function mint(address _to, uint256 _amount) external;
 }
