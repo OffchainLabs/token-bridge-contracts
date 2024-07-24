c6a94af258c56eed55357e23bf769d86c0098866
Goran Vladika <goran.vladika@gmail.com>
2024-07-24 15:04:01 +0200

    Test CI

--- contracts/tokenbridge/ethereum/L1AtomicTokenBridgeCreator.sol
+++ contracts/tokenbridge/ethereum/L1AtomicTokenBridgeCreator.sol
@@ -57,6 +57,7 @@ contract L1AtomicTokenBridgeCreator is Initializable, OwnableUpgradeable {
     error L1AtomicTokenBridgeCreator_RollupOwnershipMisconfig();
     error L1AtomicTokenBridgeCreator_ProxyAdminNotFound();
     error L1AtomicTokenBridgeCreator_L2FactoryCannotBeChanged();
+    error TestError();
 
     event OrbitTokenBridgeCreated(
         address indexed inbox,
