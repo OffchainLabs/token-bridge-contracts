5d5d860709187aa8713a50032d14ef5fa3955b53
Goran Vladika <goran.vladika@gmail.com>
2024-07-26 17:09:55 +0200

    Init gateways

--- scripts/deployment/deployUsdcBridge.ts
+++ scripts/deployment/deployUsdcBridge.ts
@@ -38,27 +38,43 @@ main().then(() => console.log('Done.'))
 async function main() {
   const { deployerL1, deployerL2 } = await _loadWallets()
 
-  const proxyAdminL2 = await _deployL2ProxyAdmin(deployerL2)
+  const proxyAdminL1 = await _deployProxyAdmin(deployerL1)
+  console.log('L1 ProxyAdmin address: ', proxyAdminL1)
+
+  const proxyAdminL2 = await _deployProxyAdmin(deployerL2)
   console.log('L2 ProxyAdmin address: ', proxyAdminL2)
 
   const bridgedUsdc = await _deployBridgedUsdc(deployerL2, proxyAdminL2)
   console.log('Bridged USDC address: ', bridgedUsdc)
 
-  /// create L2 USDC from Circle's repo, set the address in .env and load it here
-  // const l2Usdc = process.env['BRIDGED_USDC_ADDRESS'] as string
+  const l1UsdcGateway = await _deployParentChainUsdcGateway(
+    deployerL1,
+    proxyAdminL1
+  )
+  console.log('L1 USDC gateway address: ', l1UsdcGateway.address)
 
-  // const { l1USDCCustomGateway, l2USDCCustomGateway } = await _deployGateways(
-  //   deployerL1,
-  //   deployerL2
-  // )
+  const l2UsdcGateway = await _deployChildChainUsdcGateway(
+    deployerL2,
+    proxyAdminL2
+  )
+  console.log('L2 USDC gateway address: ', l2UsdcGateway.address)
 
-  // await _initializeGateways(
-  //   l1USDCCustomGateway,
-  //   l2USDCCustomGateway,
-  //   l2Usdc,
-  //   deployerL1,
-  //   deployerL2
-  // )
+  const l1Router = process.env['L1_ROUTER'] as string
+  const l2Router = process.env['L2_ROUTER'] as string
+  const inbox = process.env['INBOX'] as string
+  const l1Usdc = process.env['L1_USDC'] as string
+  await _initializeGateways(
+    l1UsdcGateway,
+    l2UsdcGateway,
+    l1Router,
+    l2Router,
+    inbox,
+    l1Usdc,
+    bridgedUsdc,
+    deployerL1,
+    deployerL2
+  )
+  console.log('Usdc gateways initialized')
 }
 
 async function _loadWallets(): Promise<{
@@ -83,21 +99,21 @@ async function _loadWallets(): Promise<{
   return { deployerL1, deployerL2 }
 }
 
-async function _deployL2ProxyAdmin(deployerL2Wallet: Wallet): Promise<string> {
-  const proxyAdminFac = await new ProxyAdmin__factory(deployerL2Wallet).deploy()
-  return (await proxyAdminFac.deployed()).address
+async function _deployProxyAdmin(deployer: Wallet): Promise<ProxyAdmin> {
+  const proxyAdminFac = await new ProxyAdmin__factory(deployer).deploy()
+  return await proxyAdminFac.deployed()
 }
 
 async function _deployBridgedUsdc(
   deployerL2Wallet: Wallet,
-  proxyAdminL2: string
+  proxyAdminL2: ProxyAdmin
 ): Promise<string> {
   /// create l2 usdc behind proxy
   const l2UsdcLogic = await _deployUsdcLogic(deployerL2Wallet)
   const l2UsdcProxyAddress = await _deployUsdcProxy(
     deployerL2Wallet,
     l2UsdcLogic.address,
-    proxyAdminL2
+    proxyAdminL2.address
   )
 
   const l2UsdcFiatToken = IFiatToken__factory.connect(
@@ -183,16 +199,10 @@ async function _deployUsdcProxy(
   return usdcProxy.address
 }
 
-async function _deployGateways(
+async function _deployParentChainUsdcGateway(
   deployerL1: Wallet,
-  deployerL2: Wallet
-): Promise<{
-  l1USDCCustomGateway: L1USDCGateway
-  l2USDCCustomGateway: L2USDCGateway
-}> {
-  /// create new L1 usdc gateway behind proxy
-  const proxyAdminFac = await new ProxyAdmin__factory(deployerL1).deploy()
-  const proxyAdmin = await proxyAdminFac.deployed()
+  proxyAdmin: ProxyAdmin
+): Promise<L1USDCGateway> {
   const l1USDCCustomGatewayFactory = await new L1USDCGateway__factory(
     deployerL1
   ).deploy()
@@ -201,141 +211,59 @@ async function _deployGateways(
     deployerL1
   ).deploy(l1USDCCustomGatewayLogic.address, proxyAdmin.address, '0x')
   const tup = await tupFactory.deployed()
-  const l1USDCCustomGateway = L1USDCGateway__factory.connect(
-    tup.address,
-    deployerL1
-  )
-  console.log('L1 ProxyAdmin address: ', proxyAdmin.address)
-  console.log('L1USDCGateway address: ', l1USDCCustomGateway.address)
+  return L1USDCGateway__factory.connect(tup.address, deployerL1)
+}
 
-  /// create new L2 usdc gateway behind proxy
-  const proxyAdminL2Fac = await new ProxyAdmin__factory(deployerL2).deploy()
-  const proxyAdminL2 = await proxyAdminL2Fac.deployed()
+async function _deployChildChainUsdcGateway(
+  deployerL2: Wallet,
+  proxyAdmin: ProxyAdmin
+): Promise<L2USDCGateway> {
   const l2USDCCustomGatewayFactory = await new L2USDCGateway__factory(
     deployerL2
   ).deploy()
   const l2USDCCustomGatewayLogic = await l2USDCCustomGatewayFactory.deployed()
-  const tupL2Factory = await new TransparentUpgradeableProxy__factory(
+  const tupFactory = await new TransparentUpgradeableProxy__factory(
     deployerL2
-  ).deploy(l2USDCCustomGatewayLogic.address, proxyAdminL2.address, '0x')
-  const tupL2 = await tupL2Factory.deployed()
-  const l2USDCCustomGateway = L2USDCGateway__factory.connect(
-    tupL2.address,
-    deployerL2
-  )
-  console.log('L2 ProxyAdmin address: ', proxyAdminL2.address)
-  console.log('L2USDCGateway address: ', l2USDCCustomGateway.address)
-
-  return { l1USDCCustomGateway, l2USDCCustomGateway }
+  ).deploy(l2USDCCustomGatewayLogic.address, proxyAdmin.address, '0x')
+  const tup = await tupFactory.deployed()
+  return L2USDCGateway__factory.connect(tup.address, deployerL2)
 }
 
 async function _initializeGateways(
-  l1USDCCustomGateway: L1USDCGateway,
-  l2USDCCustomGateway: L2USDCGateway,
+  l1UsdcGateway: L1USDCGateway,
+  l2UsdcGateway: L2USDCGateway,
+  l1Router: string,
+  l2Router: string,
+  inbox: string,
+  l1USDC: string,
   l2Usdc: string,
   deployerL1: Wallet,
   deployerL2: Wallet
 ) {
   /// initialize L1 gateway
-  const _l2CounterPart = l2USDCCustomGateway.address
-  const _l1Router = '0xcE18836b233C83325Cc8848CA4487e94C6288264'
-  const _inbox = '0xaAe29B0366299461418F5324a79Afc425BE5ae21'
-  const _l1USDC = '0xf4FEa76b87D9bCedE79EB29bBD5EDAefcA0E7dcA'
-  const _l2USDC = l2Usdc
+  const _l2CounterPart = l2UsdcGateway.address
   const _owner = deployerL1.address
   await (
-    await l1USDCCustomGateway.initialize(
+    await l1UsdcGateway.initialize(
       _l2CounterPart,
-      _l1Router,
-      _inbox,
-      _l1USDC,
-      _l2USDC,
+      l1Router,
+      inbox,
+      l1USDC,
+      l2Usdc,
       _owner
     )
   ).wait()
-  console.log('L1 USDC custom gateway initialized')
 
   /// initialize L2 gateway
-  const _l1Counterpart = l1USDCCustomGateway.address
-  const _l2Router = '0x9fDD1C4E4AA24EEc1d913FABea925594a20d43C7'
-  const _ownerL2 = deployerL2.address
+  const _l1Counterpart = l1UsdcGateway.address
+  const ownerL2 = deployerL2.address
   await (
-    await l2USDCCustomGateway.initialize(
+    await l2UsdcGateway.initialize(
       _l1Counterpart,
-      _l2Router,
-      _l1USDC,
-      _l2USDC,
-      _ownerL2
+      l2Router,
+      l1USDC,
+      l2Usdc,
+      ownerL2
     )
   ).wait()
-  console.log('L2 USDC custom gateway initialized')
-}
-
-async function _print(deployerL1Wallet: Wallet, deployerL2Wallet: Wallet) {
-  /// register USDC custom gateway
-  const router = L1GatewayRouter__factory.connect(
-    '0xcE18836b233C83325Cc8848CA4487e94C6288264',
-    deployerL1Wallet
-  )
-  const l2Router = L2GatewayRouter__factory.connect(
-    '0x9fDD1C4E4AA24EEc1d913FABea925594a20d43C7',
-    deployerL2Wallet
-  )
-
-  const l1Usdc = '0xf4FEa76b87D9bCedE79EB29bBD5EDAefcA0E7dcA'
-  const l1USDCCustomGateway = '0xE6C04c453BE367AAEF8E8bab26E5Ce6366Be3Fe6'
-  const l2USDCCustomGateway = '0xCC083dDee542023FC7CE8Cf52C8666519B981ec2'
-
-  const l1ToL2MsgGasEstimate = new L1ToL2MessageGasEstimator(
-    deployerL2Wallet.provider
-  )
-
-  //   const payload = abi.encodeWithSelector(
-  //     L2GatewayRouter.setGateway.selector,
-  //     _token,
-  //     _gateway
-  // );
-  const iface = new ethers.utils.Interface([
-    'function setGateway(address _token, address _gateway)',
-  ])
-  const payload = iface.encodeFunctionData('setGateway', [
-    l1Usdc,
-    l1USDCCustomGateway,
-  ])
-
-  const gasParams = await l1ToL2MsgGasEstimate.estimateAll(
-    {
-      from: l1USDCCustomGateway,
-      to: l2USDCCustomGateway,
-      l2CallValue: BigNumber.from(0),
-      excessFeeRefundAddress: deployerL1Wallet.address,
-      callValueRefundAddress: deployerL1Wallet.address,
-      data: payload,
-    },
-    await getBaseFee(deployerL1Wallet.provider!),
-    deployerL1Wallet.provider!
-  )
-  console.log('Gas params: ', gasParams)
-
-  // const maxGas = BigNumber.from(1)
-  // const gasPriceBid = BigNumber.from(1)
-  // let maxSubmissionCost = BigNumber.from(257600000000)
-  // const registrationCalldata = router.interface.encodeFunctionData(
-  //   'setGateways',
-  //   [[l1Usdc], [l1USDCCustomGateway], maxGas, gasPriceBid, maxSubmissionCost]
-  // )
-
-  // const upExec = UpgradeExecutor__factory.connect(
-  //   '0x5FEe78FE9AD96c1d8557C6D6BB22Eb5A61eeD315',
-  //   deployerL1Wallet
-  // )
-  // const gwRegistrationTx = await upExec.executeCall(
-  //   router.address,
-  //   registrationCalldata,
-  //   {
-  //     value: maxGas.mul(gasPriceBid).add(maxSubmissionCost),
-  //   }
-  // )
-  // await waitOnL2Msg(gwRegistrationTx)
-  // console.log('USDC custom gateway registered')
 }
