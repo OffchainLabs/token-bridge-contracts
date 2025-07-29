4bbf4653d06e525372013c0d45138f331813011b
Henry <11198460+godzillaba@users.noreply.github.com>
2025-07-29 12:17:31 -0500

    fix

--- scripts/storage_layout_test.bash
+++ scripts/storage_layout_test.bash
@@ -4,7 +4,7 @@ for CONTRACTNAME in L1ERC20Gateway L1CustomGateway L1ReverseCustomGateway L1Weth
 do
     echo "Checking storage change of $CONTRACTNAME"
     [ -f "$output_dir/$CONTRACTNAME" ] && mv "$output_dir/$CONTRACTNAME" "$output_dir/$CONTRACTNAME-old"
-    forge inspect "$CONTRACTNAME" --pretty storage > "$output_dir/$CONTRACTNAME"
+    forge inspect "$CONTRACTNAME" storage > "$output_dir/$CONTRACTNAME"
     diff "$output_dir/$CONTRACTNAME-old" "$output_dir/$CONTRACTNAME"
     if [[ $? != "0" ]]
     then
