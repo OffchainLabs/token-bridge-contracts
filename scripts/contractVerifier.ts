5a273afe1a4b6123fd5295ae6b49c803c903ca07
TucksonDev <tucksondev@gmail.com>
2025-08-21 12:50:27 +0100

    Add verification of contracts

--- scripts/contractVerifier.ts
+++ scripts/contractVerifier.ts
@@ -1,9 +1,8 @@
 import { exec } from 'child_process'
-import { ethers } from 'ethers'
 
 export class ContractVerifier {
   chainId: number
-  apiKey: string = ''
+  apiKey = ''
 
   readonly NUM_OF_OPTIMIZATIONS = 100
   readonly COMPILER_VERSION = '0.8.16'
@@ -17,41 +16,40 @@ export class ContractVerifier {
     'node_modules/@offchainlabs/upgrade-executor/src/UpgradeExecutor.sol:UpgradeExecutor'
 
   readonly contractToSource = {
-    l1TokenBridgeCreatorProxyAdmin: this.PROXY_ADMIN,
-    l1TokenBridgeCreatorLogic:
+    ProxyAdmin: this.PROXY_ADMIN,
+    TransparentUpgradeableProxy: this.TUP,
+    UpgradeExecutor: this.EXECUTOR,
+    L1AtomicTokenBridgeCreator:
       'contracts/tokenbridge/ethereum/L1AtomicTokenBridgeCreator.sol:L1AtomicTokenBridgeCreator',
-    l1TokenBridgeCreatorProxy: this.TUP,
-    retryableSenderLogic:
+    L1TokenBridgeRetryableSender:
       'contracts/tokenbridge/ethereum/L1TokenBridgeRetryableSender.sol:L1TokenBridgeRetryableSender',
-    retryableSenderProxy: this.TUP,
-    routerTemplate:
+    L1GatewayRouter:
       'contracts/tokenbridge/ethereum/gateway/L1GatewayRouter.sol:L1GatewayRouter',
-    standardGatewayTemplate:
+    L1ERC20Gateway:
       'contracts/tokenbridge/ethereum/gateway/L1ERC20Gateway.sol:L1ERC20Gateway',
-    customGatewayTemplate:
+    L1CustomGateway:
       'contracts/tokenbridge/ethereum/gateway/L1CustomGateway.sol:L1CustomGateway',
-    wethGatewayTemplate:
+    L1WethGateway:
       'contracts/tokenbridge/ethereum/gateway/L1WethGateway.sol:L1WethGateway',
-    feeTokenBasedRouterTemplate:
+    L1OrbitGatewayRouter:
       'contracts/tokenbridge/ethereum/gateway/L1OrbitGatewayRouter.sol:L1OrbitGatewayRouter',
-    feeTokenBasedStandardGatewayTemplate:
+    L1OrbitERC20Gateway:
       'contracts/tokenbridge/ethereum/gateway/L1OrbitERC20Gateway.sol:L1OrbitERC20Gateway',
-    feeTokenBasedCustomGatewayTemplate:
+    L1OrbitCustomGateway:
       'contracts/tokenbridge/ethereum/gateway/L1OrbitCustomGateway.sol:L1OrbitCustomGateway',
-    upgradeExecutor: this.EXECUTOR,
-    l2TokenBridgeFactoryOnL1:
+    L2AtomicTokenBridgeFactory:
       'contracts/tokenbridge/arbitrum/L2AtomicTokenBridgeFactory.sol:L2AtomicTokenBridgeFactory',
-    l2GatewayRouterOnL1:
+    L2GatewayRouter:
       'contracts/tokenbridge/arbitrum/gateway/L2GatewayRouter.sol:L2GatewayRouter',
-    l2StandardGatewayAddressOnL1:
+    L2ERC20Gateway:
       'contracts/tokenbridge/arbitrum/gateway/L2ERC20Gateway.sol:L2ERC20Gateway',
-    l2CustomGatewayAddressOnL1:
+    L2CustomGateway:
       'contracts/tokenbridge/arbitrum/gateway/L2CustomGateway.sol:L2CustomGateway',
-    l2WethGatewayAddressOnL1:
+    L2WethGateway:
       'contracts/tokenbridge/arbitrum/gateway/L2WethGateway.sol:L2WethGateway',
-    l2WethAddressOnL1: 'contracts/tokenbridge/libraries/aeWETH.sol:aeWETH',
-    l2MulticallAddressOnL1: 'contracts/rpc-utils/MulticallV2.sol:ArbMulticall2',
-    l1Multicall: 'contracts/rpc-utils/MulticallV2.sol:Multicall2',
+    AeWETH: 'contracts/tokenbridge/libraries/aeWETH.sol:aeWETH',
+    ArbMulticall2: 'contracts/rpc-utils/MulticallV2.sol:ArbMulticall2',
+    Multicall2: 'contracts/rpc-utils/MulticallV2.sol:Multicall2',
   }
 
   constructor(chainId: number, apiKey: string) {
@@ -66,10 +64,7 @@ export class ContractVerifier {
     contractAddress: string,
     constructorArgs?: string,
     _numOfOptimization?: number
-  ) {
-    // avoid rate limiting
-    await new Promise(resolve => setTimeout(resolve, 1000))
-
+  ): Promise<void> {
     let command = `forge verify-contract --chain-id ${this.chainId} --compiler-version ${this.COMPILER_VERSION}`
 
     if (_numOfOptimization !== undefined) {
@@ -86,17 +81,20 @@ export class ContractVerifier {
     }
     command = `${command} ${contractAddress} ${sourceFile} --etherscan-api-key ${this.apiKey}`
 
+    console.log(`Verifying ${name} at ${contractAddress}...`)
     exec(command, (err: Error | null, stdout: string, stderr: string) => {
-      console.log('-----------------')
-      console.log(command)
       if (err) {
+        console.log('-----------------')
         console.log(
-          'Failed to submit for verification',
-          contractAddress,
+          ` * Contract ${name} at ${contractAddress} failed verification`,
+          command,
           stderr
         )
+        console.log('-----------------')
       } else {
-        console.log('Successfully submitted for verification', contractAddress)
+        console.log(
+          ` * Contract ${name} at ${contractAddress} was successfully verified`
+        )
       }
     })
   }
