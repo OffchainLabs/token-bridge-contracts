b6289fe8be5150561d158f7745889509d4bf50c3
Henry <11198460+godzillaba@users.noreply.github.com>
2025-09-19 11:03:23 -0700

    add ownership to MVF

--- contracts/tokenbridge/libraries/vault/MasterVaultFactory.sol
+++ contracts/tokenbridge/libraries/vault/MasterVaultFactory.sol
@@ -3,21 +3,23 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Create2.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "./IMasterVault.sol";
 import "./MasterVault.sol";
 
-contract MasterVaultFactory {
-    event VaultDeployed(address indexed token, address indexed gateway, address vault);
+contract MasterVaultFactory is OwnableUpgradeable {
+    event VaultDeployed(address indexed token, address indexed vault);
 
-    error VaultDeploymentFailed();
     error ZeroAddress();
+
+    function initialize(address _owner) public initializer {
+        _transferOwnership(_owner);
+    }
 
     function deployVault(address token) public returns (address vault) {
         if (token == address(0)) {
             revert ZeroAddress();
         }
-
-        address gateway = msg.sender;
 
         bytes memory bytecode = abi.encodePacked(
             type(MasterVault).creationCode,
@@ -26,7 +28,7 @@ contract MasterVaultFactory {
 
         vault = Create2.deploy(0, bytes32(0), bytecode);
 
-        emit VaultDeployed(token, gateway, vault);
+        emit VaultDeployed(token, vault);
     }
 
     function calculateVaultAddress(
