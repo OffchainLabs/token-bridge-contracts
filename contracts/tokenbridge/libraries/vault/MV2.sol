4d8711f6d22dddd38ce2a48d474b191567cf7d93
Henry <11198460+godzillaba@users.noreply.github.com>
2025-09-24 12:29:34 -0600

    simplify

--- contracts/tokenbridge/libraries/vault/MV2.sol
+++ contracts/tokenbridge/libraries/vault/MV2.sol
@@ -12,25 +12,59 @@ contract MV2 is ERC4626, Ownable {
     using SafeERC20 for IERC20;
     using Math for uint256;
 
-    ERC4626 public subVault;
+    // todo: avoid inflation, rounding, other common 4626 vulns
 
-    // how many subVault shares one MV2 share can be redeemed for
-    // initially 1 to 1
-    // constant per subvault
-    // changes when subvault is set
-    // todo: this initial rate should be whatever makes 1mstUSDC == 1USDC, not 1mstUSDC == 1stUSDC
-    uint256 public subVaultExchRateWad = 1e18;
+    ERC4626 public subVault;
 
     event SubvaultChanged(address indexed oldSubvault, address indexed newSubvault);
 
     constructor(IERC20 _asset, string memory _name, string memory _symbol) ERC20(_name, _symbol) ERC4626(_asset) Ownable() {}
 
-    function masterSharesToSubShares(uint256 masterShares, Math.Rounding rounding) public view returns (uint256) {
-        return masterShares.mulDiv(subVaultExchRateWad, 1e18, rounding);
+    function setSubVault(ERC4626 _subVault, uint256 minSubVaultShares) external onlyOwner {
+        require(address(subVault) == address(0), "subvault already set");
+        require(totalSupply() > 0, "must have supply before setting subvault");
+
+        // deposit to subvault
+        IERC20(asset()).safeApprove(address(_subVault), type(uint256).max);
+        uint256 subShares = _subVault.deposit(totalAssets(), address(this));
+        require(subShares >= minSubVaultShares, "too few subvault shares");
+
+        // set subvault
+        subVault = _subVault;
+
+        emit SubvaultChanged(address(0), address(_subVault));
     }
 
+    function switchSubVault(ERC4626 newSubVault, uint256 minAssetReceived, uint256 minNewSubVaultShares) external onlyOwner {
+        ERC4626 oldSubVault = subVault;
+        require(address(oldSubVault) != address(0), "no existing subvault");
+
+        // withdraw from old subvault
+        uint256 assetReceived = oldSubVault.withdraw(oldSubVault.maxWithdraw(address(this)), address(this), address(this));
+        require(assetReceived >= minAssetReceived, "too few assets received");
+
+        // revoke approval from old subvault
+        IERC20(asset()).safeApprove(address(oldSubVault), 0);
+
+        // deposit to new subvault
+        if (address(newSubVault) != address(0)) {
+            IERC20(asset()).safeApprove(address(newSubVault), type(uint256).max);
+            uint256 newSubShares = newSubVault.deposit(totalAssets(), address(this));
+            require(newSubShares >= minNewSubVaultShares, "too few new subvault shares");
+        }
+
+        // set subvault
+        subVault = newSubVault;
+
+        emit SubvaultChanged(address(oldSubVault), address(newSubVault));
+    }
+
+    // todo: make these handle a zero subvault
+    function masterSharesToSubShares(uint256 masterShares, Math.Rounding rounding) public view returns (uint256) {
+        return masterShares.mulDiv(subVault.totalSupply(), totalSupply(), rounding);
+    }
     function subSharesToMasterShares(uint256 subShares, Math.Rounding rounding) public view returns (uint256) {
-        return subShares.mulDiv(1e18, subVaultExchRateWad, rounding);
+        return subShares.mulDiv(totalSupply(), subVault.totalSupply(), rounding);
     }
 
     /** @dev See {IERC4626-totalAssets}. */
@@ -54,31 +88,6 @@ contract MV2 is ERC4626, Ownable {
             return type(uint256).max;
         }
         return subSharesToMasterShares(subShares, Math.Rounding.Down);
-    }
-
-    /**
-     * @dev Internal conversion function (from assets to shares) with support for rounding direction.
-     *
-     * Will revert if assets > 0, totalSupply > 0 and totalAssets = 0. That corresponds to a case where any asset
-     * would represent an infinite amount of shares.
-     */
-    function _convertToShares(uint256 assets, Math.Rounding rounding) internal view virtual override returns (uint256 shares) {
-        ERC4626 _subVault = subVault;
-        if (address(_subVault) == address(0)) {
-            return super._convertToShares(assets, rounding);
-        }
-        return subSharesToMasterShares(_subVault.convertToShares(assets), rounding);
-    }
-
-    /**
-     * @dev Internal conversion function (from shares to assets) with support for rounding direction.
-     */
-    function _convertToAssets(uint256 shares, Math.Rounding rounding) internal view virtual override returns (uint256 assets) {
-        ERC4626 _subVault = subVault;
-        if (address(_subVault) == address(0)) {
-            return super._convertToAssets(shares, rounding);
-        }
-        return _subVault.convertToAssets(masterSharesToSubShares(shares, rounding));
     }
 
     /**
