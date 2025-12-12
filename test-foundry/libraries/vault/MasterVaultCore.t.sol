69345c1ca7d63a34f8ed211c89d4496d390ce8da
Henry <11198460+godzillaba@users.noreply.github.com>
2025-12-12 14:38:50 -0500

    use factory in core test setup

--- test-foundry/libraries/vault/MasterVaultCore.t.sol
+++ test-foundry/libraries/vault/MasterVaultCore.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import { Test } from "forge-std/Test.sol";
 import { MasterVault } from "../../../contracts/tokenbridge/libraries/vault/MasterVault.sol";
+import { MasterVaultFactory } from "../../../contracts/tokenbridge/libraries/vault/MasterVaultFactory.sol";
 import { TestERC20 } from "../../../contracts/tokenbridge/test/TestERC20.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { UpgradeableBeacon } from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
@@ -13,10 +14,9 @@ import {
 import { IAccessControl } from "@openzeppelin/contracts/access/IAccessControl.sol";
 
 contract MasterVaultCoreTest is Test {
+    MasterVaultFactory public factory;
     MasterVault public vault;
     TestERC20 public token;
-    UpgradeableBeacon public beacon;
-    BeaconProxyFactory public beaconProxyFactory;
 
     address public user = address(0x1);
     string public name = "Master Test Token";
@@ -27,18 +27,9 @@ contract MasterVaultCoreTest is Test {
     }
 
     function setUp() public virtual {
+        factory = new MasterVaultFactory();
+        factory.initialize(address(this));
         token = new TestERC20();
-
-        MasterVault implementation = new MasterVault();
-        beacon = new UpgradeableBeacon(address(implementation));
-
-        beaconProxyFactory = new BeaconProxyFactory();
-        beaconProxyFactory.initialize(address(beacon));
-
-        bytes32 salt = keccak256("test");
-        address proxyAddress = beaconProxyFactory.createProxy(salt);
-        vault = MasterVault(proxyAddress);
-
-        vault.initialize(IERC20(address(token)), name, symbol, address(this));
+        vault = MasterVault(factory.deployVault(address(token)));
     }
 }
