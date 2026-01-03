f4a9ea62a511bbe8749fe95bdbec10f193f7cce2
Wael Almattar <waelsy123@gmail.com>
2026-01-03 16:08:04 +0100

    tests: move util methods into core MasterVaultCoreTests contract

--- test-foundry/libraries/vault/MasterVault.t.sol
+++ test-foundry/libraries/vault/MasterVault.t.sol
@@ -6,24 +6,11 @@ import { MockSubVault } from "../../../contracts/tokenbridge/test/MockSubVault.s
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
-import {console2} from "forge-std/console2.sol";
 
 contract MasterVaultFirstDepositTest is MasterVaultCoreTest {
     using Math for uint256;
 
-    uint256 constant FRESH_STATE_PLACEHOLDER = uint256(keccak256("FRESH_STATE_PLACEHOLDER"));
-    uint256 constant DEAD_SHARES = 10**18;
-
-    struct State {
-        uint256 userShares;
-        uint256 masterVaultTotalAssets;
-        uint256 masterVaultTotalSupply;
-        uint256 masterVaultTokenBalance;
-        uint256 masterVaultSubVaultShareBalance;
-        uint256 subVaultTotalAssets;
-        uint256 subVaultTotalSupply;
-        uint256 subVaultTokenBalance;
-    }
+    uint256 constant DEAD_SHARES = 10 ** 18;
 
     // first deposit
     function test_deposit(uint96 _depositAmount) public {
@@ -33,16 +20,18 @@ contract MasterVaultFirstDepositTest is MasterVaultCoreTest {
         token.approve(address(vault), depositAmount);
         uint256 shares = vault.deposit(depositAmount, user);
         vm.stopPrank();
-        _checkState(State({
-            userShares: depositAmount * DEAD_SHARES,
-            masterVaultTotalAssets: depositAmount,
-            masterVaultTotalSupply: (1 + depositAmount) * DEAD_SHARES,
-            masterVaultTokenBalance: depositAmount,
-            masterVaultSubVaultShareBalance: 0,
-            subVaultTotalAssets: 0,
-            subVaultTotalSupply: 0,
-            subVaultTokenBalance: 0
-        }));
+        _checkState(
+            State({
+                userShares: depositAmount * DEAD_SHARES,
+                masterVaultTotalAssets: depositAmount,
+                masterVaultTotalSupply: (1 + depositAmount) * DEAD_SHARES,
+                masterVaultTokenBalance: depositAmount,
+                masterVaultSubVaultShareBalance: 0,
+                subVaultTotalAssets: 0,
+                subVaultTotalSupply: 0,
+                subVaultTokenBalance: 0
+            })
+        );
         assertEq(shares, depositAmount * DEAD_SHARES, "shares mismatch deposit return value");
     }
 
@@ -53,16 +42,18 @@ contract MasterVaultFirstDepositTest is MasterVaultCoreTest {
         token.approve(address(vault), mintAmount);
         uint256 assets = vault.mint(mintAmount, user);
         vm.stopPrank();
-        _checkState(State({
-            userShares: mintAmount,
-            masterVaultTotalAssets: mintAmount.ceilDiv(1e18),
-            masterVaultTotalSupply: mintAmount + DEAD_SHARES,
-            masterVaultTokenBalance: mintAmount.ceilDiv(1e18),
-            masterVaultSubVaultShareBalance: 0,
-            subVaultTotalAssets: 0,
-            subVaultTotalSupply: 0,
-            subVaultTokenBalance: 0
-        }));
+        _checkState(
+            State({
+                userShares: mintAmount,
+                masterVaultTotalAssets: mintAmount.ceilDiv(1e18),
+                masterVaultTotalSupply: mintAmount + DEAD_SHARES,
+                masterVaultTokenBalance: mintAmount.ceilDiv(1e18),
+                masterVaultSubVaultShareBalance: 0,
+                subVaultTotalAssets: 0,
+                subVaultTotalSupply: 0,
+                subVaultTokenBalance: 0
+            })
+        );
         assertEq(assets, mintAmount.ceilDiv(1e18), "assets mismatch mint return value");
     }
 
@@ -74,17 +65,23 @@ contract MasterVaultFirstDepositTest is MasterVaultCoreTest {
         vm.startPrank(user);
         uint256 sharesRedeemed = vault.withdraw(withdrawAmount, user, user);
         vm.stopPrank();
-        _checkState(State({
-            userShares: (firstDeposit - withdrawAmount) * DEAD_SHARES,
-            masterVaultTotalAssets: firstDeposit - withdrawAmount,
-            masterVaultTotalSupply: (1 + firstDeposit - withdrawAmount) * DEAD_SHARES,
-            masterVaultTokenBalance: firstDeposit - withdrawAmount,
-            masterVaultSubVaultShareBalance: 0,
-            subVaultTotalAssets: 0,
-            subVaultTotalSupply: 0,
-            subVaultTokenBalance: 0
-        }));
-        assertEq(sharesRedeemed, withdrawAmount * DEAD_SHARES, "sharesRedeemed mismatch withdraw return value");
+        _checkState(
+            State({
+                userShares: (firstDeposit - withdrawAmount) * DEAD_SHARES,
+                masterVaultTotalAssets: firstDeposit - withdrawAmount,
+                masterVaultTotalSupply: (1 + firstDeposit - withdrawAmount) * DEAD_SHARES,
+                masterVaultTokenBalance: firstDeposit - withdrawAmount,
+                masterVaultSubVaultShareBalance: 0,
+                subVaultTotalAssets: 0,
+                subVaultTotalSupply: 0,
+                subVaultTokenBalance: 0
+            })
+        );
+        assertEq(
+            sharesRedeemed,
+            withdrawAmount * DEAD_SHARES,
+            "sharesRedeemed mismatch withdraw return value"
+        );
     }
 
     function test_redeem(uint96 _firstMint, uint96 _redeemAmount) public {
@@ -95,59 +92,26 @@ contract MasterVaultFirstDepositTest is MasterVaultCoreTest {
         State memory beforeState = _getState();
         vm.startPrank(user);
         uint256 assets = vault.redeem(redeemAmount, user, user);
-        uint256 expectedAssets = (1 + beforeState.masterVaultTotalAssets) * redeemAmount / (beforeState.masterVaultTotalSupply);
+        uint256 expectedAssets = ((1 + beforeState.masterVaultTotalAssets) * redeemAmount) /
+            (beforeState.masterVaultTotalSupply);
         vm.stopPrank();
-        _checkState(State({
-            userShares: beforeState.userShares - redeemAmount,
-            masterVaultTotalAssets: beforeState.masterVaultTotalAssets - expectedAssets,
-            masterVaultTotalSupply: beforeState.masterVaultTotalSupply - redeemAmount,
-            masterVaultTokenBalance: beforeState.masterVaultTokenBalance - expectedAssets,
-            masterVaultSubVaultShareBalance: 0,
-            subVaultTotalAssets: 0,
-            subVaultTotalSupply: 0,
-            subVaultTokenBalance: 0
-        }));
+        _checkState(
+            State({
+                userShares: beforeState.userShares - redeemAmount,
+                masterVaultTotalAssets: beforeState.masterVaultTotalAssets - expectedAssets,
+                masterVaultTotalSupply: beforeState.masterVaultTotalSupply - redeemAmount,
+                masterVaultTokenBalance: beforeState.masterVaultTokenBalance - expectedAssets,
+                masterVaultSubVaultShareBalance: 0,
+                subVaultTotalAssets: 0,
+                subVaultTotalSupply: 0,
+                subVaultTokenBalance: 0
+            })
+        );
         assertEq(assets, expectedAssets, "assets mismatch redeem return value");
-    }
-
-    function _checkState(State memory expectedState) internal {
-        assertEq(expectedState.userShares, vault.balanceOf(user), "userShares mismatch");
-        assertEq(expectedState.masterVaultTotalAssets, vault.totalAssets(), "masterVaultTotalAssets mismatch");
-        assertEq(expectedState.masterVaultTotalSupply, vault.totalSupply(), "masterVaultTotalSupply mismatch");
-        assertEq(expectedState.masterVaultTokenBalance, token.balanceOf(address(vault)), "masterVaultTokenBalance mismatch");
-        assertEq(expectedState.masterVaultSubVaultShareBalance, vault.subVault().balanceOf(address(vault)), "masterVaultSubVaultShareBalance mismatch");
-        assertEq(expectedState.subVaultTotalAssets, vault.subVault().totalAssets(), "subVaultTotalAssets mismatch");
-        assertEq(expectedState.subVaultTotalSupply, vault.subVault().totalSupply(), "subVaultTotalSupply mismatch");
-        assertEq(expectedState.subVaultTokenBalance, token.balanceOf(address(vault.subVault())), "subVaultTokenBalance mismatch");
-    }
-
-    function _getState() internal view returns (State memory) {
-        return State({
-            userShares: vault.balanceOf(user),
-            masterVaultTotalAssets: vault.totalAssets(),
-            masterVaultTotalSupply: vault.totalSupply(),
-            masterVaultTokenBalance: token.balanceOf(address(vault)),
-            masterVaultSubVaultShareBalance: vault.subVault().balanceOf(address(vault)),
-            subVaultTotalAssets: vault.subVault().totalAssets(),
-            subVaultTotalSupply: vault.subVault().totalSupply(),
-            subVaultTokenBalance: token.balanceOf(address(vault.subVault()))
-        });
-    }
-
-    function _logState(string memory label, State memory state) internal view {
-        console2.log(label);
-        console2.log(" userShares:", state.userShares);
-        console2.log(" masterVaultTotalAssets:", state.masterVaultTotalAssets);
-        console2.log(" masterVaultTotalSupply:", state.masterVaultTotalSupply);
-        console2.log(" masterVaultTokenBalance:", state.masterVaultTokenBalance);
-        console2.log(" masterVaultSubVaultShareBalance:", state.masterVaultSubVaultShareBalance);
-        console2.log(" subVaultTotalAssets:", state.subVaultTotalAssets);
-        console2.log(" subVaultTotalSupply:", state.subVaultTotalSupply);
-        console2.log(" subVaultTokenBalance:", state.subVaultTokenBalance);
     }
 }
 
-// contract MasterVaultTestWithSubvaultFresh is MasterVaultTest {
+// contract MasterVaultTestWithSubvaultFresh is MasterVaultFirstDepositTest {
 //     function setUp() public override {
 //         super.setUp();
 //         MockSubVault _subvault = new MockSubVault(IERC20(address(token)), "TestSubvault", "TSV");
@@ -155,7 +119,7 @@ contract MasterVaultFirstDepositTest is MasterVaultCoreTest {
 //     }
 // }
 
-// contract MasterVaultTestWithSubvaultHoldingAssets is MasterVaultTest {
+// contract MasterVaultTestWithSubvaultHoldingAssets is MasterVaultFirstDepositTest {
 //     function setUp() public override {
 //         super.setUp();
 
--- test-foundry/libraries/vault/MasterVaultCore.t.sol
+++ test-foundry/libraries/vault/MasterVaultCore.t.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.8.0;
 
 import { Test } from "forge-std/Test.sol";
 import { MasterVault } from "../../../contracts/tokenbridge/libraries/vault/MasterVault.sol";
-import { MasterVaultFactory } from "../../../contracts/tokenbridge/libraries/vault/MasterVaultFactory.sol";
+import {
+    MasterVaultFactory
+} from "../../../contracts/tokenbridge/libraries/vault/MasterVaultFactory.sol";
 import { TestERC20 } from "../../../contracts/tokenbridge/test/TestERC20.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { UpgradeableBeacon } from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
@@ -12,6 +14,7 @@ import {
     ClonableBeaconProxy
 } from "../../../contracts/tokenbridge/libraries/ClonableBeaconProxy.sol";
 import { IAccessControl } from "@openzeppelin/contracts/access/IAccessControl.sol";
+import { console2 } from "forge-std/console2.sol";
 
 contract MasterVaultCoreTest is Test {
     MasterVaultFactory public factory;
@@ -22,6 +25,17 @@ contract MasterVaultCoreTest is Test {
     string public name = "Master Test Token";
     string public symbol = "mTST";
 
+    struct State {
+        uint256 userShares;
+        uint256 masterVaultTotalAssets;
+        uint256 masterVaultTotalSupply;
+        uint256 masterVaultTokenBalance;
+        uint256 masterVaultSubVaultShareBalance;
+        uint256 subVaultTotalAssets;
+        uint256 subVaultTotalSupply;
+        uint256 subVaultTokenBalance;
+    }
+
     function getAssetsHoldingVault() internal view virtual returns (address) {
         return address(vault.subVault()) == address(0) ? address(vault) : address(vault.subVault());
     }
@@ -31,5 +45,70 @@ contract MasterVaultCoreTest is Test {
         factory.initialize(address(this));
         token = new TestERC20();
         vault = MasterVault(factory.deployVault(address(token)));
+    }
+
+    function _checkState(State memory expectedState) internal {
+        assertEq(expectedState.userShares, vault.balanceOf(user), "userShares mismatch");
+        assertEq(
+            expectedState.masterVaultTotalAssets,
+            vault.totalAssets(),
+            "masterVaultTotalAssets mismatch"
+        );
+        assertEq(
+            expectedState.masterVaultTotalSupply,
+            vault.totalSupply(),
+            "masterVaultTotalSupply mismatch"
+        );
+        assertEq(
+            expectedState.masterVaultTokenBalance,
+            token.balanceOf(address(vault)),
+            "masterVaultTokenBalance mismatch"
+        );
+        assertEq(
+            expectedState.masterVaultSubVaultShareBalance,
+            vault.subVault().balanceOf(address(vault)),
+            "masterVaultSubVaultShareBalance mismatch"
+        );
+        assertEq(
+            expectedState.subVaultTotalAssets,
+            vault.subVault().totalAssets(),
+            "subVaultTotalAssets mismatch"
+        );
+        assertEq(
+            expectedState.subVaultTotalSupply,
+            vault.subVault().totalSupply(),
+            "subVaultTotalSupply mismatch"
+        );
+        assertEq(
+            expectedState.subVaultTokenBalance,
+            token.balanceOf(address(vault.subVault())),
+            "subVaultTokenBalance mismatch"
+        );
+    }
+
+    function _getState() internal view returns (State memory) {
+        return
+            State({
+                userShares: vault.balanceOf(user),
+                masterVaultTotalAssets: vault.totalAssets(),
+                masterVaultTotalSupply: vault.totalSupply(),
+                masterVaultTokenBalance: token.balanceOf(address(vault)),
+                masterVaultSubVaultShareBalance: vault.subVault().balanceOf(address(vault)),
+                subVaultTotalAssets: vault.subVault().totalAssets(),
+                subVaultTotalSupply: vault.subVault().totalSupply(),
+                subVaultTokenBalance: token.balanceOf(address(vault.subVault()))
+            });
+    }
+
+    function _logState(string memory label, State memory state) internal view {
+        console2.log(label);
+        console2.log(" userShares:", state.userShares);
+        console2.log(" masterVaultTotalAssets:", state.masterVaultTotalAssets);
+        console2.log(" masterVaultTotalSupply:", state.masterVaultTotalSupply);
+        console2.log(" masterVaultTokenBalance:", state.masterVaultTokenBalance);
+        console2.log(" masterVaultSubVaultShareBalance:", state.masterVaultSubVaultShareBalance);
+        console2.log(" subVaultTotalAssets:", state.subVaultTotalAssets);
+        console2.log(" subVaultTotalSupply:", state.subVaultTotalSupply);
+        console2.log(" subVaultTokenBalance:", state.subVaultTokenBalance);
     }
 }
