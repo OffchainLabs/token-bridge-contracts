3a1b9862449d220a6b55b8fa78b08878f6c61a6a
Henry <11198460+godzillaba@users.noreply.github.com>
2026-01-13 15:20:34 -0700

    document roles

--- contracts/tokenbridge/libraries/vault/MasterVaultRoles.sol
+++ contracts/tokenbridge/libraries/vault/MasterVaultRoles.sol
@@ -9,25 +9,47 @@ import {
 ///         Each MasterVault will have a reference to a singleton MasterVaultRoles contract, in addition to inheriting MasterVaultRoles directly.
 ///         This allows for easier management of roles across multiple vaults.
 contract MasterVaultRoles is AccessControlEnumerableUpgradeable {
-    /// @notice Subvault manager role can set/revoke subvaults, set target allocation, and set minimum rebalance amount
-    /// @dev    Should never be granted to the zero address
-    bytes32 public constant SUBVAULT_MANAGER_ROLE = keccak256("SUBVAULT_MANAGER_ROLE");
-    /// @notice Fee manager role can toggle performance fees and set the performance fee beneficiary
-    /// @dev    Should never be granted to the zero address
+    /// @notice The admin can:
+    ///         - Grant/revoke all roles (besides DEFAULT_ADMIN_ROLE, which is unused)
+    ///         - Add/remove whitelisted subvaults
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    /// @notice The general manager can:
+    ///         - Grant/revoke GENERAL_MANAGER_ROLE, FEE_MANAGER_ROLE, PAUSER_ROLE, and KEEPER_ROLE
+    ///         - Set the subVault to any whitelisted subVault
+    ///         - Set the target allocation
+    ///         - Set the minimum rebalance amount
+    bytes32 public constant GENERAL_MANAGER_ROLE = keccak256("GENERAL_MANAGER_ROLE");
+    /// @notice The fee manager can:
+    ///         - Toggle performance fees on/off
+    ///         - Set the performance fee beneficiary
     bytes32 public constant FEE_MANAGER_ROLE = keccak256("FEE_MANAGER_ROLE");
-    /// @notice Pauser role can pause/unpause deposits and withdrawals (todo: pause should pause EVERYTHING)
+    /// @notice The pauser can:
+    ///         - pause/unpause deposits and withdrawals
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-    /// @notice Keeper role can rebalance the vault and distribute performance fees
-    bytes32 public constant KEEPER_ROLE = keccak256("KEEPER");
+    /// @notice The keeper can:
+    ///         - rebalance 
+    ///         - distribute performance fees
+    bytes32 public constant KEEPER_ROLE = keccak256("KEEPER_ROLE");
 
     function __MasterVaultRoles_init() internal onlyInitializing {
         __AccessControlEnumerable_init();
     }
 
     function initialize(address admin) external initializer {
-        _grantRole(DEFAULT_ADMIN_ROLE, admin);
-        _grantRole(FEE_MANAGER_ROLE, admin);
-        _grantRole(PAUSER_ROLE, admin);
-        _grantRole(KEEPER_ROLE, admin);
+        // set ADMIN_ROLE as admin of all roles
+        _setRoleAdmin(ADMIN_ROLE, ADMIN_ROLE);
+        _setRoleAdmin(GENERAL_MANAGER_ROLE, ADMIN_ROLE);
+        _setRoleAdmin(FEE_MANAGER_ROLE, ADMIN_ROLE);
+        _setRoleAdmin(PAUSER_ROLE, ADMIN_ROLE);
+        _setRoleAdmin(KEEPER_ROLE, ADMIN_ROLE);
+
+        // set GENERAL_MANAGER_ROLE as admin of appropriate roles
+        _setRoleAdmin(GENERAL_MANAGER_ROLE, GENERAL_MANAGER_ROLE);
+        _setRoleAdmin(FEE_MANAGER_ROLE, GENERAL_MANAGER_ROLE);
+        _setRoleAdmin(PAUSER_ROLE, GENERAL_MANAGER_ROLE);
+        _setRoleAdmin(KEEPER_ROLE, GENERAL_MANAGER_ROLE);
+
+        // grant ADMIN_ROLE to admin
+        _grantRole(ADMIN_ROLE, admin);
     }
 }
