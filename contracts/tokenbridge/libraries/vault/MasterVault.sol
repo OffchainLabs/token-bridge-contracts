cbda9e160e181b3624e071258169f4301444f3ea
Wael Almattar <waelsy123@gmail.com>
2026-02-03 14:01:22 +0100

    doc: add a comment to require underlying to implemet decimals() method

--- contracts/tokenbridge/libraries/vault/MasterVault.sol
+++ contracts/tokenbridge/libraries/vault/MasterVault.sol
@@ -454,6 +454,7 @@ contract MasterVault is
     }
 
     /// @dev Overridden to add EXTRA_DECIMALS to the underlying asset decimals
+    /// @notice Requires underlying asset to implement IERC20Metadata.decimals()
     function decimals() public view override returns (uint8) {
         return IERC20Metadata(address(asset)).decimals() + EXTRA_DECIMALS;
     }
