fb019c4b42c0c213fd5018f0b7c9928f382cb1bb
Wael Almattar <waelsy123@gmail.com>
2026-02-04 13:14:02 +0100

    fixup! add master vault contracts

--- contracts/tokenbridge/libraries/vault/MasterVault.sol
+++ contracts/tokenbridge/libraries/vault/MasterVault.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import {MasterVaultRoles} from "./MasterVaultRoles.sol";
 import {
     ERC20Upgradeable
-} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+} from "contracts/tokenbridge/libraries/ERC20Upgradeable.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {
     IERC20Upgradeable
