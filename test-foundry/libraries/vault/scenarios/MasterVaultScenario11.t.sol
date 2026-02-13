2b0c724f5e1a5c45d2eaf1d032c47061ab3c3640
Wael Almattar <waelsy123@gmail.com>
2026-02-13 03:34:25 +0100

    fixup! test: more rebalance tests

--- test-foundry/libraries/vault/scenarios/MasterVaultScenario11.t.sol
+++ test-foundry/libraries/vault/scenarios/MasterVaultScenario11.t.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.8.0;
 
 import {MasterVaultScenarioCoreTest} from "./MasterVaultScenarioCore.t.sol";
 
-/// This test proofs that rebalance keeps the profit in the subvault.
+/// This test proofs that rebalance withdraws all assets including profit from the subvault.
 contract MasterVaultScenario11Test is MasterVaultScenarioCoreTest {
     /// @dev Scenario: 50% allocation, sub vault doubles in value, then allocation set to 0%
     /// User A deposits 400, 50% allocated to sub vault, sub vault doubles (100% profit)
-    /// Allocation changed to 0%, rebalance withdraws only principal
-    function test_scenario11_profitRemainsInSubVault() public {
+    /// Allocation changed to 0%, rebalance withdraws all assets including profit
+    function test_scenario11_profitWithdrawnFromSubVault() public {
         vault.setTargetAllocationWad(0.5e18);
 
         _mintTokens(userA, 400);
@@ -43,11 +43,9 @@ contract MasterVaultScenario11Test is MasterVaultScenarioCoreTest {
         vm.warp(block.timestamp + 2);
         vault.rebalance(0);
 
-        // profit remains in subvault
-        assertGt(
-            vault.subVault().balanceOf(address(vault)),
-            0,
-            "Vault should still hold sub vault shares"
+        // all assets withdrawn from subvault including profit
+        assertEq(
+            vault.subVault().balanceOf(address(vault)), 0, "Vault should hold no sub vault shares"
         );
 
         _checkState(
@@ -55,16 +53,14 @@ contract MasterVaultScenario11Test is MasterVaultScenarioCoreTest {
                 userShares: 400 * DEAD_SHARES,
                 masterVaultTotalAssets: 600,
                 masterVaultTotalSupply: 401 * DEAD_SHARES,
-                masterVaultTokenBalance: 401,
-                masterVaultSubVaultShareBalance: 99,
-                subVaultTotalAssets: 198,
-                subVaultTotalSupply: 99,
-                subVaultTokenBalance: 198
+                masterVaultTokenBalance: 599,
+                masterVaultSubVaultShareBalance: 0,
+                subVaultTotalAssets: 0,
+                subVaultTotalSupply: 0,
+                subVaultTokenBalance: 0
             })
         );
 
-        assertEq(
-            token.balanceOf(address(vault.subVault())), 198, "Profit should remain in sub vault"
-        );
+        assertEq(token.balanceOf(address(vault.subVault())), 0, "Sub vault should have no tokens");
     }
 }
