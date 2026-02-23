b9ec81ce1f96a17fcacac8fec2f72ce612432735
Henry <11198460+godzillaba@users.noreply.github.com>
2026-02-23 10:57:26 -0700

    default subvault tests moved

--- test-foundry/libraries/vault/MasterVaultMutationKill.t.sol
+++ test-foundry/libraries/vault/MasterVaultMutationKill.t.sol
@@ -59,39 +59,6 @@ contract MasterVaultMutationKillTest is MasterVaultCoreTest {
         assertTrue(vault.supportsInterface(type(IAccessControlUpgradeable).interfaceId));
     }
 
-    // ====== MasterVaultFactory.sol (DefaultSubVault) ======
-
-    /// TARGETS MUTANT #5 in MasterVaultFactory.sol
-    function test_defaultSubVault_withdraw_onlyMasterVault() public {
-        DefaultSubVault dsv = DefaultSubVault(address(vault.subVault()));
-        address attacker = address(0xdead);
-        vm.prank(attacker);
-        vm.expectRevert("ONLY_MASTER_VAULT");
-        dsv.withdraw(1, attacker, attacker);
-    }
-
-    /// TARGETS MUTANT #6 in MasterVaultFactory.sol
-    function test_defaultSubVault_withdraw_requireTrue_onlyMasterVault() public {
-        DefaultSubVault dsv = DefaultSubVault(address(vault.subVault()));
-        vm.prank(address(vault));
-        // should not revert when called by masterVault (with 0 amount)
-        dsv.withdraw(0, address(vault), address(vault));
-    }
-
-    /// TARGETS MUTANT #8 in MasterVaultFactory.sol
-    function test_defaultSubVault_mint_reverts() public {
-        DefaultSubVault dsv = DefaultSubVault(address(vault.subVault()));
-        vm.expectRevert("UNSUPPORTED");
-        dsv.mint(1, address(this));
-    }
-
-    /// TARGETS MUTANT #9 in MasterVaultFactory.sol
-    function test_defaultSubVault_redeem_reverts() public {
-        DefaultSubVault dsv = DefaultSubVault(address(vault.subVault()));
-        vm.expectRevert("UNSUPPORTED");
-        dsv.redeem(1, address(this), address(this));
-    }
-
     // ====== MasterVault.sol - initialize ======
 
     /// TARGETS MUTANT #1 in MasterVault.sol
--- test-foundry/libraries/vault/mutation/MasterVaultDefaultSubVault.t.sol
+++ test-foundry/libraries/vault/mutation/MasterVaultDefaultSubVault.t.sol
@@ -7,5 +7,30 @@ import {
 } from "../../../../contracts/tokenbridge/libraries/vault/MasterVaultFactory.sol";
 
 contract MasterVaultDefaultSubVaultTest is MasterVaultMutationBase {
-    // Move DefaultSubVault tests here (mutants #5, #6, #8, #9 in MasterVaultFactory.sol)
+    function test_defaultSubVault_withdraw_onlyMasterVault() public {
+        DefaultSubVault dsv = DefaultSubVault(address(vault.subVault()));
+        address attacker = address(0xdead);
+        vm.prank(attacker);
+        vm.expectRevert("ONLY_MASTER_VAULT");
+        dsv.withdraw(1, attacker, attacker);
+    }
+
+    function test_defaultSubVault_withdraw_requireTrue_onlyMasterVault() public {
+        DefaultSubVault dsv = DefaultSubVault(address(vault.subVault()));
+        vm.prank(address(vault));
+        // should not revert when called by masterVault (with 0 amount)
+        dsv.withdraw(0, address(vault), address(vault));
+    }
+
+    function test_defaultSubVault_mint_reverts() public {
+        DefaultSubVault dsv = DefaultSubVault(address(vault.subVault()));
+        vm.expectRevert("UNSUPPORTED");
+        dsv.mint(1, address(this));
+    }
+
+    function test_defaultSubVault_redeem_reverts() public {
+        DefaultSubVault dsv = DefaultSubVault(address(vault.subVault()));
+        vm.expectRevert("UNSUPPORTED");
+        dsv.redeem(1, address(this), address(this));
+    }
 }
